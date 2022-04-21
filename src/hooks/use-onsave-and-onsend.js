56a8b5e8c1edde70f2c7ab3411be829005cb5bd3
gabriele <gabriele.marino@zextras.com>
2022-04-21 12:48:32 +0200

    feat: informative snackbars on save appointment

--- src/hooks/use-onsave-and-onsend.js
+++ src/hooks/use-onsave-and-onsend.js
@@ -3,10 +3,17 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import { useCallback } from 'react';
+import { useCallback, useContext, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useRemoveCurrentBoard, replaceHistory, useUserAccount } from '@zextras/carbonio-shell-ui';
+import {
+	useRemoveCurrentBoard,
+	replaceHistory,
+	useUserAccount,
+	FOLDERS
+} from '@zextras/carbonio-shell-ui';
 import { startsWith } from 'lodash';
+import { SnackbarManagerContext } from '@zextras/carbonio-design-system';
+import { useTranslation } from 'react-i18next';
 import { editAppointmentData } from '../store/slices/editor-slice';
 import { selectEditor } from '../store/selectors/editor';
 import { createAppointment } from '../store/actions/new-create-appointment';
@@ -17,6 +24,9 @@ export const useOnSaveAndOnSend = (id, isBoard) => {
 	const account = useUserAccount();
 	const closeBoard = useRemoveCurrentBoard();
 	const editor = useSelector((state) => selectEditor(state, id));
+	const createSnackbar = useContext(SnackbarManagerContext);
+	const [t] = useTranslation();
+	const [isIdle, setIsIdle] = useState(true);
 
 	const saveAppointment = useCallback(
 		(data) =>
@@ -31,9 +41,29 @@ export const useOnSaveAndOnSend = (id, isBoard) => {
 	}, [isBoard, closeBoard]);
 
 	const onSave = useCallback(() => {
-		dispatch(editAppointmentData({ id, mod: { resource: { draft: true } } }));
-		dispatch(saveAppointment({ ...editor, resource: { ...editor.resource, draft: true } }));
-	}, [dispatch, id, saveAppointment, editor]);
+		if (isIdle) {
+			setIsIdle(false);
+			dispatch(editAppointmentData({ id, mod: { resource: { draft: true } } }));
+			dispatch(saveAppointment({ ...editor, resource: { ...editor.resource, draft: true } })).then(
+				(res) => {
+					if (res?.type) {
+						const success = res.type.includes('fulfilled');
+						createSnackbar({
+							key: `calendar-moved-root`,
+							replace: true,
+							type: success ? 'info' : 'warning',
+							hideButton: true,
+							label: !success
+								? t('label.error_try_again', 'Something went wrong, please try again')
+								: t('message.snackbar.calendar_edits_saved', 'Edits saved correctly'),
+							autoHideTimeout: 3000
+						});
+					}
+					setIsIdle(true);
+				}
+			);
+		}
+	}, [isIdle, dispatch, id, saveAppointment, editor, createSnackbar, t]);
 
 	const onSend = useCallback(() => {
 		dispatch(
