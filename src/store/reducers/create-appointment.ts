aa871953655375043771fe3833814f75131e883f
Gabriele Marino <62339254+nubsthead@users.noreply.github.com>
2022-05-10 10:31:34 +0200

    fix: user can modify appointments after creation (#63)
    * fix: user can modify appointments after creation

--- src/store/reducers/create-appointment.ts
+++ src/store/reducers/create-appointment.ts
@@ -1,13 +1,11 @@
 /*
- * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const handleCreateAppointmentFulfilled = (state: any, { payload }: any): any => {
-	const { id, appt, panel } = payload;
-	state.editors[id] = appt;
-	if (panel) {
-		state.editorPanel = payload.id;
-	}
+export const createAppointmentFulfilled = (state: any, { payload }: any): void => {
+	state.editors[payload.editor.resource.id].resource.id = payload.response.calItemId;
+	state.editors[payload.editor.resource.id].resource.inviteId = payload.response.invId;
 };
