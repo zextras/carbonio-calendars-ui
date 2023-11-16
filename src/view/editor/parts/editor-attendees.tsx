bec8abd080befb0f12526dcf1c52ff9ad803bce4
Gabriele Marino <62339254+nubsthead@users.noreply.github.com>
2023-11-16 16:32:38 +0100

    fix: remove chip custom component from editor attendees field

--- src/view/editor/parts/editor-attendees.tsx
+++ src/view/editor/parts/editor-attendees.tsx
@@ -3,20 +3,20 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import {
-	Button,
-	ChipInput,
-	Text,
-	Container,
-	Row,
-	ChipInputProps
-} from '@zextras/carbonio-design-system';
-import { useIntegratedComponent } from '@zextras/carbonio-shell-ui';
-import { find, intersectionBy, map, some } from 'lodash';
 import React, { ReactElement, useCallback, useMemo, useState } from 'react';
+
+import { Button, ChipInput, Text, Container, Row } from '@zextras/carbonio-design-system';
+import { useIntegratedComponent } from '@zextras/carbonio-shell-ui';
+import { find, intersectionBy, isNil, map, some, startsWith } from 'lodash';
+import moment from 'moment';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import { useAttendeesAvailability } from '../../../hooks/use-attendees-availability';
+
+import {
+	AttendeesAvailabilityListType,
+	AttendeesAvailabilityType,
+	useAttendeesAvailability
+} from '../../../hooks/use-attendees-availability';
 import { useAppDispatch, useAppSelector } from '../../../store/redux/hooks';
 import {
 	selectEditorAllDay,
@@ -31,7 +31,7 @@ import {
 	editEditorAttendees,
 	editEditorOptionalAttendees
 } from '../../../store/slices/editor-slice';
-import { AttendeesChip, getIsBusyAtTimeOfTheEvent } from './attendees-chip';
+import { Editor } from '../../../types/editor';
 
 type EditorAttendeesProps = {
 	editorId: string;
@@ -51,12 +51,67 @@ export const AttendeesContainer = styled.div`
 	}
 `;
 
+export const getIsBusyAtTimeOfTheEvent = (
+	item: AttendeesAvailabilityType,
+	start: Editor['start'],
+	end: Editor['end'],
+	attendeesAvailabilityList: AttendeesAvailabilityListType,
+	isAllDay: Editor['allDay']
+): boolean => {
+	if (start && end && attendeesAvailabilityList && !isNil(isAllDay)) {
+		const startDate = moment(start);
+		const endDate = moment(end);
+
+		const startDay = startDate.format('DDMMYYYY');
+		const endDay = endDate.format('DDMMYYYY');
+		const diffInDays = endDate.diff(startDate, 'days');
+		const startHour = startDate.format('HHmm');
+		const endHour = endDate.format('HHmm');
+
+		if (isAllDay) {
+			const diffInDaysAllDay = endDate
+				.add(1, 'day')
+				.startOf('day')
+				.diff(startDate.startOf('day'), 'days');
+			return diffInDaysAllDay > 1
+				? false
+				: !!find(
+						(item.t ?? []).concat(item.b ?? []),
+						(slot) =>
+							moment(slot.s).format('DD') === startDate.format('DD') ||
+							moment(slot.e).format('DD') === startDate.format('DD')
+				  );
+		}
+
+		// from 00:00 to 00:00
+		const lastsEntireDay = diffInDays === 1 && startHour === endHour && startsWith(startHour, '00');
+		const endsTheSameDay = startDay === endDay;
+		if (lastsEntireDay || endsTheSameDay) {
+			return !!find(
+				(item.t ?? []).concat(item.b ?? []),
+				(slot) =>
+					// appointment starts while attendee is busy
+					(start > slot.s && start < slot.e) ||
+					// the appointment ends while attendee is busy
+					(end > slot.s && end < slot.e) ||
+					// the appointment starts before the attendee is busy and ends after the attendee is busy
+					(start < slot.s && end > slot.e) ||
+					// the appointment starts when the attendee has another appointment starting at the same time
+					start === slot.s ||
+					// the appointment ends when the attendee has another appointment ending at the same time
+					end === slot.e
+			);
+		}
+	}
+
+	return false;
+};
+
 export const EditorAttendees = ({ editorId }: EditorAttendeesProps): ReactElement => {
 	const [t] = useTranslation();
 	const [ContactInput, integrationAvailable] = useIntegratedComponent('contact-input');
 	const [showOptionals, setShowOptional] = useState(false);
 	const toggleOptionals = useCallback(() => setShowOptional((show) => !show), []);
-
 	const dispatch = useAppDispatch();
 	const attendees = useAppSelector(selectEditorAttendees(editorId));
 	const uid = useAppSelector(selectEditorUid(editorId));
@@ -76,13 +131,14 @@ export const EditorAttendees = ({ editorId }: EditorAttendeesProps): ReactElemen
 
 	const onChange = useCallback(
 		(value) => {
+			const _attendees = map(value, (chip) => ({
+				...chip,
+				email: chip?.email ?? chip?.label
+			}));
 			dispatch(
 				editEditorAttendees({
 					id: editorId,
-					attendees: map(value, (chip) => ({
-						...chip,
-						email: chip?.email ?? chip?.label
-					}))
+					attendees: _attendees
 				})
 			);
 		},
@@ -107,21 +163,48 @@ export const EditorAttendees = ({ editorId }: EditorAttendeesProps): ReactElemen
 			),
 		[allDay, attendeesAvailabilityList, availableChips, end, start]
 	);
+	const defaultValue = useMemo(() => {
+		if (attendees?.length > 0) {
+			return map(attendees, (chip) => {
+				const chipLabel = chip.email ?? chip.label;
+				const currentChipAvailability = find(attendeesAvailabilityList, ['email', chipLabel]);
 
-	const CustomChipComponent = useCallback<
-		(args: React.ComponentProps<NonNullable<ChipInputProps['ChipComponent']>>) => JSX.Element
-	>(
-		(props) => (
-			<AttendeesChip
-				{...props}
-				start={start}
-				allDay={allDay}
-				end={end}
-				attendeesAvailabilityList={attendeesAvailabilityList}
-			/>
-		),
-		[start, allDay, end, attendeesAvailabilityList]
-	);
+				if (currentChipAvailability) {
+					const isBusyAtTimeOfEvent = getIsBusyAtTimeOfTheEvent(
+						currentChipAvailability,
+						start,
+						end,
+						attendeesAvailabilityList,
+						allDay
+					);
+					const actions = isBusyAtTimeOfEvent
+						? [
+								{
+									id: 'unavailable',
+									label: t(
+										'attendee_unavailable',
+										'Attendee not available at the selected time of the event'
+									),
+									color: 'error',
+									type: 'icon',
+									icon: 'AlertTriangle'
+								} as const
+						  ]
+						: undefined;
+					return {
+						...chip,
+						email: chip?.email ?? chip?.label,
+						actions
+					};
+				}
+				return {
+					...chip,
+					email: chip?.email ?? chip?.label
+				};
+			});
+		}
+		return [];
+	}, [allDay, attendees, attendeesAvailabilityList, end, start, t]);
 
 	return (
 		<>
@@ -139,8 +222,7 @@ export const EditorAttendees = ({ editorId }: EditorAttendeesProps): ReactElemen
 								// @ts-ignore
 								placeholder={t('label.attendee_plural', 'Attendees')}
 								onChange={onChange}
-								defaultValue={attendees}
-								ChipComponent={CustomChipComponent}
+								defaultValue={defaultValue}
 								disabled={disabled?.attendees}
 							/>
 						) : (
@@ -148,10 +230,9 @@ export const EditorAttendees = ({ editorId }: EditorAttendeesProps): ReactElemen
 								placeholder={t('label.attendee_plural', 'Attendees')}
 								background={'gray5'}
 								onChange={onChange}
-								defaultValue={attendees}
+								defaultValue={defaultValue}
 								hasError={hasError}
 								errorLabel=""
-								ChipComponent={CustomChipComponent}
 								disabled={disabled?.attendees}
 							/>
 						)}
