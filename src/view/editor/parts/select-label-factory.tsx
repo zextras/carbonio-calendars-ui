14d279660273b1c5f95085fd623c804d38c36224
Gabriele Marino <62339254+nubsthead@users.noreply.github.com>
2023-11-20 16:11:55 +0100

    feat: showing calendars' owners inside editor calendar selector

--- src/view/editor/parts/select-label-factory.tsx
+++ src/view/editor/parts/select-label-factory.tsx
@@ -3,7 +3,8 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useMemo } from 'react';
+
 import {
 	Container,
 	Padding,
@@ -13,9 +14,16 @@ import {
 	TextProps,
 	IconProps,
 	SelectItem,
-	LabelFactoryProps
+	LabelFactoryProps,
+	Tooltip
 } from '@zextras/carbonio-design-system';
+import { Grant } from '@zextras/carbonio-shell-ui/types/misc';
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
+
+import { useRoot } from '../../../carbonio-ui-commons/store/zustand/folder';
+import { FOLDERS } from '../../../carbonio-ui-commons/test/mocks/carbonio-shell-ui-constants';
+import { isLinkChild } from '../../../commons/utilities';
 
 export const Square = styled.div`
 	width: 1rem;
@@ -31,8 +39,14 @@ export const ColorContainer = styled(Container)`
 
 export const TextUpperCase = styled(Text)`
 	text-transform: capitalize;
-	color: ${({ theme, disabled }): string =>
-		disabled ? theme.palette.text.disabled : theme.palette.text.regular};
+	text-overflow: ellipsis;
+	white-space: nowrap;
+	color: ${({ theme, disabled, color }): string => {
+		if (color) {
+			return disabled ? theme.palette.text.disabled : color;
+		}
+		return disabled ? theme.palette.text.disabled : theme.palette.text.regular;
+	}};
 `;
 
 export const LabelText = styled(Text)<TextProps & { $showPrimary?: boolean }>`
@@ -54,6 +68,10 @@ color: ${({ theme, disabled, $showPrimary }): string =>
 		: theme.palette.secondary.regular}};`;
 
 interface CustomSelectItem extends SelectItem {
+	id?: string;
+	acl?: { grant: Array<Grant> };
+	isLink?: boolean;
+	absFolderPath?: string | undefined;
 	color?: string;
 }
 
@@ -61,50 +79,122 @@ interface CustomLabelFactoryProps extends LabelFactoryProps {
 	selected: CustomSelectItem[];
 }
 
-const LabelFactory = ({
-	selected,
+const RowWithIcon = ({
+	icon,
+	color,
+	tooltipText
+}: {
+	icon: string;
+	color: string;
+	tooltipText: string;
+}): JSX.Element => (
+	<Padding left="small">
+		<Tooltip placement="right" label={tooltipText}>
+			<Row>
+				<Icon icon={icon} color={color} size="medium" />
+			</Row>
+		</Tooltip>
+	</Padding>
+);
+
+export const ItemFactory = ({
+	id,
+	color,
 	label,
-	open,
-	focus,
+	acl,
+	isLink,
+	absFolderPath,
 	disabled
-}: CustomLabelFactoryProps): ReactElement => (
-	<ColorContainer
-		orientation="horizontal"
-		width="fill"
-		crossAlignment="center"
-		mainAlignment="space-between"
-		borderRadius="half"
-		padding={{
-			all: 'small'
-		}}
-		background="gray5"
-		style={{ cursor: disabled ? 'no-drop' : 'pointer' }}
-	>
-		<Row width="100%" takeAvailableSpace mainAlignment="space-between">
-			<Row
-				orientation="vertical"
-				crossAlignment="flex-start"
-				mainAlignment="flex-start"
-				padding={{ left: 'small' }}
-			>
-				<LabelText size="small" disabled={disabled} $showPrimary={open || focus}>
-					{label}
-				</LabelText>
-				<Row>
-					<Padding right="small">
-						<Square color={selected[0].color} disabled={disabled} />
+}: {
+	id: string | undefined;
+	color: string | undefined;
+	label: string;
+	acl?: { grant: Array<Grant> };
+	isLink?: boolean;
+	absFolderPath?: string | undefined;
+	disabled: boolean;
+}): JSX.Element => {
+	const [t] = useTranslation();
+	const root = useRoot(id ?? '');
+	const sharedStatusIcon = useMemo(() => {
+		if (isLink || isLinkChild({ absFolderPath })) {
+			const tooltipText = t('tooltip.folder_linked_status', 'Linked to me');
+			return <RowWithIcon icon={'Linked'} color={'linked'} tooltipText={tooltipText} />;
+		}
+		if (acl?.grant) {
+			const tooltipText = t('tooltip.calendar_sharing_status', {
+				count: acl?.grant?.length,
+				defaultValue_plural: 'Shared with {{count}} people',
+				defaultValue: 'Shared with 1 person'
+			});
+			return <RowWithIcon icon={'Shared'} color={'shared'} tooltipText={tooltipText} />;
+		}
+		return null;
+	}, [absFolderPath, acl?.grant, isLink, t]);
+	return (
+		<Row wrap={'nowrap'}>
+			<Padding right="small">
+				<Square color={color} disabled={disabled} />
+			</Padding>
+			<TextUpperCase disabled={disabled}>{label}</TextUpperCase>
+			{sharedStatusIcon}
+			<Row takeAvailableSpace>
+				{root && root.id !== FOLDERS.USER_ROOT && (
+					<Padding left="small" style={{ overflow: 'hidden' }}>
+						<TextUpperCase color={'gray1'}>{`(${root.name})`}</TextUpperCase>
 					</Padding>
-					<TextUpperCase disabled={disabled}>{selected[0].label}</TextUpperCase>
-				</Row>
+				)}
 			</Row>
 		</Row>
-		<StyledIcon
-			size="large"
-			icon={open ? 'ChevronUpOutline' : 'ChevronDownOutline'}
-			disabled={disabled}
-			$showPrimary={open || focus}
-			style={{ alignSelf: 'center' }}
-		/>
-	</ColorContainer>
-);
+	);
+};
+
+const LabelFactory = (item: CustomLabelFactoryProps): ReactElement => {
+	const { selected, label, open, focus, disabled } = item;
+	return (
+		<ColorContainer
+			orientation="horizontal"
+			width="fill"
+			crossAlignment="center"
+			mainAlignment="space-between"
+			borderRadius="half"
+			padding={{
+				all: 'small'
+			}}
+			background={'gray5'}
+			style={{ cursor: disabled ? 'no-drop' : 'pointer' }}
+		>
+			<Row width="100%" takeAvailableSpace mainAlignment="space-between">
+				<Row
+					orientation="vertical"
+					crossAlignment="flex-start"
+					mainAlignment="flex-start"
+					padding={{ left: 'small' }}
+				>
+					<LabelText size="small" disabled={disabled} $showPrimary={open || focus}>
+						{label}
+					</LabelText>
+					{selected?.[0] && (
+						<ItemFactory
+							label={selected[0].label}
+							color={selected[0].color}
+							isLink={selected[0].isLink}
+							absFolderPath={selected[0].absFolderPath}
+							acl={selected[0].acl}
+							id={selected[0].id}
+							disabled={disabled}
+						/>
+					)}
+				</Row>
+			</Row>
+			<StyledIcon
+				size="large"
+				icon={open ? 'ChevronUpOutline' : 'ChevronDownOutline'}
+				disabled={disabled}
+				$showPrimary={open || focus}
+				style={{ alignSelf: 'center' }}
+			/>
+		</ColorContainer>
+	);
+};
 export default LabelFactory;
