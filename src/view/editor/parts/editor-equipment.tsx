de5874ad0d49e0432f2c514ca0a75f4b673d2f4f
gabriele <gabriele.marino@zextras.com>
2023-12-01 12:34:13 +0100

    feat: equipment availability

--- src/view/editor/parts/editor-equipment.tsx
+++ src/view/editor/parts/editor-equipment.tsx
@@ -3,14 +3,34 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { ReactElement, useCallback, useEffect, useState } from 'react';
+import React, { ReactElement, useCallback, useEffect, useState, useMemo } from 'react';
 
-import { Select } from '@zextras/carbonio-design-system';
+import {
+	Container,
+	Icon,
+	Padding,
+	Row,
+	Select,
+	Tooltip,
+	Text
+} from '@zextras/carbonio-design-system';
 import { compact, find, map, xorBy } from 'lodash';
 import { useTranslation } from 'react-i18next';
 
+import {
+	EditorAvailabilityWarningRow,
+	getIsBusyAtTimeOfTheEvent
+} from './editor-availability-warning-row';
+import { useAttendeesAvailability } from '../../../hooks/use-attendees-availability';
 import { useAppDispatch, useAppSelector } from '../../../store/redux/hooks';
-import { selectEditorEquipment, selectEditorDisabled } from '../../../store/selectors/editor';
+import {
+	selectEditorEquipment,
+	selectEditorDisabled,
+	selectEditorEnd,
+	selectEditorStart,
+	selectEditorAllDay,
+	selectEditorUid
+} from '../../../store/selectors/editor';
 import { editEditorEquipment } from '../../../store/slices/editor-slice';
 import { useEquipments } from '../../../store/zustand/hooks';
 import { Resource } from '../../../types/editor';
@@ -23,6 +43,13 @@ export const EditorEquipment = ({ editorId }: { editorId: string }): ReactElemen
 	const [selection, setSelection] = useState<Array<Resource> | undefined>(undefined);
 	const disabled = useAppSelector(selectEditorDisabled(editorId));
 	const dispatch = useAppDispatch();
+
+	const start = useAppSelector(selectEditorStart(editorId));
+	const end = useAppSelector(selectEditorEnd(editorId));
+	const allDay = useAppSelector(selectEditorAllDay(editorId));
+	const uid = useAppSelector(selectEditorUid(editorId));
+
+	const attendeesAvailabilityList = useAttendeesAvailability(start, equipments, uid);
 
 	const onChange = useCallback(
 		(e) => {
@@ -49,15 +76,75 @@ export const EditorEquipment = ({ editorId }: { editorId: string }): ReactElemen
 		}
 	}, [equipment, equipments, selection]);
 
-	return equipments ? (
-		<Select
-			items={equipments}
-			background={'gray5'}
-			label={t('label.equipment', 'Equipment')}
-			onChange={onChange}
-			disabled={disabled?.meetingRoom}
-			selection={selection}
-			multiple
-		/>
+	const equipmentsAvailability = useMemo(() => {
+		if (!equipments?.length) {
+			return [];
+		}
+		return map(equipments, (_equipment) => {
+			const equipmentInList = find(attendeesAvailabilityList, ['email', _equipment.email]);
+			const isSelected = find(selection, ['email', _equipment.email]);
+
+			if (equipmentInList) {
+				const isBusyAtTimeOfEvent = getIsBusyAtTimeOfTheEvent(
+					equipmentInList,
+					start,
+					end,
+					attendeesAvailabilityList,
+					allDay
+				);
+
+				if (isBusyAtTimeOfEvent) {
+					return {
+						..._equipment,
+						email: _equipment?.email ?? _equipment?.label,
+						customComponent: (
+							<Container width="fit" mainAlignment="flex-start" orientation="horizontal">
+								<Icon icon={(isSelected && 'CheckmarkSquare') || 'Square' || ''} />
+								<Padding horizontal={'small'}>
+									<Text weight={isSelected ? 'bold' : 'regular'}>{_equipment?.label}</Text>
+								</Padding>
+								<Tooltip
+									label={t(
+										'attendee_equipment_unavailable',
+										'Equipment not available at the selected time of the event'
+									)}
+								>
+									<Row>
+										<Icon icon="AlertTriangle" color="error" />
+									</Row>
+								</Tooltip>
+							</Container>
+						)
+					};
+				}
+			}
+			return {
+				..._equipment,
+				email: _equipment?.email ?? _equipment?.label
+			};
+		});
+	}, [allDay, attendeesAvailabilityList, end, equipments, selection, start, t]);
+
+	return equipmentsAvailability ? (
+		<>
+			<Select
+				items={equipmentsAvailability}
+				background={'gray5'}
+				label={t('label.equipment', 'Equipment')}
+				onChange={onChange}
+				disabled={disabled?.meetingRoom}
+				selection={selection}
+				multiple
+			/>
+			<EditorAvailabilityWarningRow
+				label={t(
+					'attendees_equipments_unavailable',
+					'One or more Equipments are not available at the selected time of the event'
+				)}
+				list={attendeesAvailabilityList}
+				items={equipment ?? []}
+				editorId={editorId}
+			/>
+		</>
 	) : null;
 };
