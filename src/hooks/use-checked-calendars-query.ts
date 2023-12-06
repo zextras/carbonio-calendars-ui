3f9fbfb7676e2a24cdf6ed4439ba44769552552a
gabriele <gabriele.marino@zextras.com>
2023-12-06 15:19:42 +0100

    fix: users cannot see appointments from broken calendars

--- src/hooks/use-checked-calendars-query.ts
+++ src/hooks/use-checked-calendars-query.ts
@@ -4,11 +4,14 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { filter, map } from 'lodash';
+
 import { useCheckedFolders } from './use-checked-folders';
+import { LinkFolder } from '../carbonio-ui-commons/types/folder';
 
 export const useCheckedCalendarsQuery = (): string => {
 	const calendars = useCheckedFolders();
-	return map(filter(calendars, ['checked', true]), (result, id) =>
-		id === 0 ? `inid:"${result.id}"` : `OR inid:"${result.id}"`
+	return map(
+		filter(calendars, (calendar) => calendar.checked === true && !(calendar as LinkFolder).broken),
+		(result, id) => (id === 0 ? `inid:"${result.id}"` : `OR inid:"${result.id}"`)
 	).join(' ');
 };
