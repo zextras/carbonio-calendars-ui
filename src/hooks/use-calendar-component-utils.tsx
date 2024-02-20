213311026bb4a3a7d37bb0486f17080d234c9106
gabriele <gabriele.marino@zextras.com>
2024-02-20 09:28:23 +0100

    fix: update edit trigger condition

--- src/hooks/use-calendar-component-utils.tsx
+++ src/hooks/use-calendar-component-utils.tsx
@@ -193,7 +193,7 @@ export const useCalendarComponentUtils = (): {
 			} else if (
 				!isEqual(event.start, start) ||
 				!isEqual(event.end, end) ||
-				(event.allDay !== !!isAllDay && (!isEqual(event.start, start) || !isEqual(event.end, end)))
+				(event.allDay !== !!isAllDay && moment(event.start).day() === moment(event.end).day())
 			) {
 				const onEntireSeries = (): void => {
 					const seriesEvent = {
