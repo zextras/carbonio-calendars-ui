0db8bf6e7eb8ebc793a57ab242cf8cd7d7cda947
gabriele <gabriele.marino@zextras.com>
2024-03-19 09:36:48 +0100

    fix: ics string uppercase

--- src/actions/calendar-actions-items.tsx
+++ src/actions/calendar-actions-items.tsx
@@ -250,7 +250,7 @@ export const exportAppointmentICSItem = ({
 }): CalendarActionsItems => ({
 	id: FOLDER_ACTIONS.EXPORT_ICS,
 	icon: 'Download',
-	label: t('action.export_calendar_ics', 'Export ics file'),
+	label: t('action.export_calendar_ics', 'Export ICS file'),
 	tooltipLabel: noPermissionLabel,
 	onClick: exportCalendarICSFn({ item }),
 	disabled: isTrashOrNestedInIt(item) || hasId(item, SIDEBAR_ITEMS.ALL_CALENDAR)
