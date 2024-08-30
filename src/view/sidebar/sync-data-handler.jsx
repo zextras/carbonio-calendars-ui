6327c40df799eaa38c63d871ac82fb4403ce3206
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-08-30 10:29:38 +0200

    refactor: add handleNotifyFolders to sync-data-handler

--- src/view/sidebar/sync-data-handler.jsx
+++ src/view/sidebar/sync-data-handler.jsx
@@ -8,6 +8,8 @@ import { useEffect, useState } from 'react';
 import { useNotify, useRefresh } from '@zextras/carbonio-shell-ui';
 import { isEmpty, reduce, forEach, sortBy } from 'lodash';
 
+import { useFolderStore } from '../../carbonio-ui-commons/store/zustand/folder';
+import { folderWorker } from '../../carbonio-ui-commons/worker';
 import { useCheckedCalendarsQuery } from '../../hooks/use-checked-calendars-query';
 import { searchAppointments } from '../../store/actions/search-appointments';
 import { useAppDispatch } from '../../store/redux/hooks';
@@ -17,6 +19,24 @@ import {
 } from '../../store/slices/appointments-slice';
 import { handleModifiedInvites } from '../../store/slices/invites-slice';
 import { useRangeEnd, useRangeStart } from '../../store/zustand/hooks';
+
+function handleFoldersNotify(notifyList, notify, worker, store) {
+	const isNotifyRelatedToFolders =
+		!isEmpty(notifyList) &&
+		(notify?.created?.folder ||
+			notify?.modified?.folder ||
+			notify.deleted ||
+			notify?.created?.link ||
+			notify?.modified?.link);
+
+	if (isNotifyRelatedToFolders) {
+		worker.postMessage({
+			op: 'notify',
+			notify,
+			state: store.getState().folders
+		});
+	}
+}
 
 export const SyncDataHandler = () => {
 	const refresh = useRefresh();
@@ -38,6 +58,8 @@ export const SyncDataHandler = () => {
 		if (notifyList.length > 0) {
 			forEach(sortBy(notifyList, 'seq'), (notify) => {
 				if (!isEmpty(notify) && (notify.seq > seq || (seq > 1 && notify.seq === 1))) {
+					handleFoldersNotify(notifyList, notify, folderWorker, useFolderStore);
+
 					if (notify.created && notify.created.appt) {
 						dispatch(searchAppointments({ spanEnd: end, spanStart: start, query }));
 					}
