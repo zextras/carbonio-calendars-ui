e67acd6dde1579f5cb71d5392d942c96aa25fe52
Keshav Bhatt <keshavnrj@gmail.com>
2024-08-30 18:00:43 +0530

    fix: use useInitializeFolders removing the old useFoldersController

--- src/app.tsx
+++ src/app.tsx
@@ -24,7 +24,7 @@ import { AnyFunction } from '@zextras/carbonio-shell-ui/lib/utils/typeUtils';
 import { useTranslation } from 'react-i18next';
 
 import { FOLDER_VIEW } from './carbonio-ui-commons/constants';
-import { useFoldersController } from './carbonio-ui-commons/hooks/use-folders-controller';
+import { useInitializeFolders } from './carbonio-ui-commons/hooks/use-initialize-folders';
 import { useFoldersMap } from './carbonio-ui-commons/store/zustand/folder';
 import { CALENDAR_APP_ID, CALENDAR_BOARD_ID, CALENDAR_ROUTE } from './constants';
 import { useOnClickNewButton } from './hooks/on-click-new-button';
@@ -166,7 +166,7 @@ const AppRegistrations = (): null => {
 };
 
 export default function App(): React.JSX.Element {
-	useFoldersController(FOLDER_VIEW.appointment);
+	useInitializeFolders(FOLDER_VIEW.appointment);
 	return (
 		<StoreProvider>
 			<AppRegistrations />
