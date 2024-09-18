1b566151526debdf449d43ed8248888bafbe86a7
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2024-09-18 16:44:23 +0200

    feat: added new group from api call

--- src/view/sidebar/custom-components/create-group-component.tsx
+++ src/view/sidebar/custom-components/create-group-component.tsx
@@ -9,15 +9,29 @@ import React, { ReactElement, useMemo } from 'react';
 import { Button, Container, useModal } from '@zextras/carbonio-design-system';
 import { t } from '@zextras/carbonio-shell-ui';
 
+import {
+	useUpdateGroups,
+	getCalendarGroups
+} from '../../../carbonio-ui-commons/store/zustand/folder';
 import { createCalendarGroupRequest } from '../../../soap/create-calendar-group-request';
 
 export const CreateGroupComponent = (): ReactElement => {
 	const { createModal, closeModal } = useModal();
 	const label = useMemo(() => t('label.create_group', 'Create New Group'), []);
+	const updateGroups = useUpdateGroups();
+	const currentGroups = getCalendarGroups();
 
 	const onClick = (): void => {
-		createCalendarGroupRequest({ name: 'New Group', calendarIds: ['1'] }).then((resp) => {
-			console.log('Group created', resp);
+		createCalendarGroupRequest({ name: 'New Group', calendarIds: ['1'] }).then((res) => {
+			console.log('Group created', res);
+			updateGroups([
+				...currentGroups,
+				{
+					id: res.group.id,
+					name: res.group.name,
+					calendarId: res.group.calendarId.map((g) => g._content)
+				}
+			]);
 		});
 	};
 
