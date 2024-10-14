f82dd4c2c9cf045c062e0b89d62d4cf3fde4fbf6
Luca Stauble <luca.stauble@gmail.com>
2024-10-14 13:06:27 +0200

    test: add test for group calendars list item

---
+++ src/view/sidebar/tests/group-calendars-list-item.test.tsx
@@ -0,0 +1,78 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React from 'react';
+
+import { ZIMBRA_STANDARD_COLORS } from '../../../carbonio-ui-commons/constants';
+import { screen, setupTest } from '../../../carbonio-ui-commons/test/test-setup';
+import { Calendar } from '../../../types/store/calendars';
+import { GroupCalendarsListItem } from '../group-calendars-list-item';
+
+describe('Group calendars list item', () => {
+	it('should render the name of the calendar', () => {
+		const calendar: Pick<Calendar, 'id' | 'name' | 'color'> = {
+			id: '1',
+			name: 'Calendar name',
+			color: {
+				background: '#fff',
+				color: '4'
+			}
+		};
+
+		setupTest(<GroupCalendarsListItem calendar={calendar} onRemove={jest.fn()} />);
+
+		expect(screen.getByText('Calendar name')).toBeVisible();
+	});
+
+	it('should render the color of the calendar', () => {
+		const calendar: Pick<Calendar, 'id' | 'name' | 'color'> = {
+			id: '1',
+			name: 'Calendar name',
+			color: {
+				background: '#fff',
+				color: '4'
+			}
+		};
+
+		setupTest(<GroupCalendarsListItem calendar={calendar} onRemove={jest.fn()} />);
+
+		expect(screen.getByTestId('colored-square')).toHaveStyleRule(
+			'background',
+			ZIMBRA_STANDARD_COLORS[Number(calendar.color.color)].hex
+		);
+	});
+
+	it('should render a button to remove the calendar', () => {
+		const calendar: Pick<Calendar, 'id' | 'name' | 'color'> = {
+			id: '1',
+			name: 'Calendar name',
+			color: {
+				background: '#fff',
+				color: '4'
+			}
+		};
+
+		setupTest(<GroupCalendarsListItem calendar={calendar} onRemove={jest.fn()} />);
+
+		expect(screen.getByRole('button', { name: /remove/i })).toBeVisible();
+	});
+
+	it('should call the callback when the remove button is clicked', async () => {
+		const calendar: Pick<Calendar, 'id' | 'name' | 'color'> = {
+			id: '1',
+			name: 'Calendar name',
+			color: {
+				background: '#fff',
+				color: '4'
+			}
+		};
+		const onRemove = jest.fn();
+
+		const { user } = setupTest(<GroupCalendarsListItem calendar={calendar} onRemove={onRemove} />);
+		await user.click(screen.getByRole('button', { name: /remove/i }));
+
+		expect(onRemove).toHaveBeenCalledWith(calendar.id);
+	});
+});
