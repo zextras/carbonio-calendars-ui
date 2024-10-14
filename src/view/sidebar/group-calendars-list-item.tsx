5ef5ab5b7f3d97cc74dba6f0c4f54a00b39ba244
Luca Stauble <luca.stauble@gmail.com>
2024-10-14 13:05:42 +0200

    feat: add group calendars list item component stub

---
+++ src/view/sidebar/group-calendars-list-item.tsx
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React from 'react';
+
+import { Calendar } from '../../types/store/calendars';
+
+export type GroupCalendarsListItemProps = {
+	calendar: Pick<Calendar, 'id' | 'name' | 'color'>;
+	onRemove: (calendarId: string) => void;
+};
+
+export const GroupCalendarsListItem = ({
+	calendar,
+	onRemove
+}: GroupCalendarsListItemProps): React.JSX.Element => <></>;
---
+++ src/view/sidebar/tests/group-calendars-list.test.tsx
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+describe('Group calendars list', () => {
+	it.todo('should render a placeholder text when there is no calendar');
+
+	it.todo('should render a list of calendars');
+});
