6f5437057661d7fd648d4a982ad687da8263938e
Luca Stauble <luca.stauble@gmail.com>
2024-10-15 12:12:03 +0200

    feat: add remove button

--- src/view/sidebar/group-calendars-list-item.tsx
+++ src/view/sidebar/group-calendars-list-item.tsx
@@ -3,9 +3,10 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 
-import { Container, Row, Text } from '@zextras/carbonio-design-system';
+import { Button, Container, Row, Text } from '@zextras/carbonio-design-system';
+import { useTranslation } from 'react-i18next';
 
 import { ZIMBRA_STANDARD_COLORS } from '../../carbonio-ui-commons/constants';
 import { Square } from '../../commons/styled-components';
@@ -20,18 +21,29 @@ export const GroupCalendarsListItem = ({
 	calendar,
 	onRemove
 }: GroupCalendarsListItemProps): React.JSX.Element => {
+	const [t] = useTranslation();
+
 	const color = useMemo<string>(() => {
 		const colorIndex = calendar.color.color ? Number(calendar.color.color) : 0;
 		return ZIMBRA_STANDARD_COLORS[colorIndex].hex;
 	}, [calendar.color.color]);
 
+	const buttonLabel = t('label.remove', 'remove');
+
+	const onButtonClick = useCallback(() => {
+		onRemove(calendar.id);
+	}, [calendar.id, onRemove]);
+
 	return (
 		<Container>
-			<Row>
+			<Row width="fit">
 				<Square data-testid="colored-square" color={color} />
 			</Row>
-			<Row>
+			<Row takeAvailableSpace>
 				<Text>{calendar.name}</Text>
+			</Row>
+			<Row width="fit">
+				<Button label={buttonLabel} onClick={onButtonClick} />
 			</Row>
 		</Container>
 	);
