fa84362f6133c2664ee54ec59d944afa123863a0
Lisa Parma <lisa.parma@zextras.com>
2024-10-23 12:14:54 +0200

    feat: add tooltip on virtual room row

--- src/view/event-summary-view/virtual-room-row.tsx
+++ src/view/event-summary-view/virtual-room-row.tsx
@@ -5,8 +5,9 @@
  */
 import React, { ReactElement, useMemo } from 'react';
 
-import { Icon, Padding, Row, Text } from '@zextras/carbonio-design-system';
+import { Icon, Padding, Row, Text, Tooltip } from '@zextras/carbonio-design-system';
 import { find } from 'lodash';
+import { useTranslation } from 'react-i18next';
 
 import { CRB_XPROPS, CRB_XPARAMS } from '../../constants/xprops';
 import { XPropProps } from '../../types/store/invite';
@@ -18,26 +19,32 @@ export const VirtualRoomRow = ({
 	xprop: XPropProps;
 	showIcon?: boolean;
 }): ReactElement | null => {
+	const [t] = useTranslation();
+	const tooltipLabel = t('appointment.virtualRoom.displayerLink', 'Join the virtual room');
+
 	const room = useMemo(() => find(xprop, ['name', CRB_XPROPS.MEETING_ROOM]), [xprop]);
+
 	if (room) {
 		const roomName = find(room.xparam, ['name', CRB_XPARAMS.ROOM_NAME])?.value;
 		const roomLink = find(room.xparam, ['name', CRB_XPARAMS.ROOM_LINK])?.value;
 		return (
 			<Row width="fill" mainAlignment="flex-start" padding={{ top: 'small' }}>
-				<Row takeAvailableSpace mainAlignment="flex-start">
-					{showIcon && (
-						<Padding right="small">
-							<Icon icon="VideoOutline" size="medium" />
-						</Padding>
-					)}
-					<Row takeAvailableSpace mainAlignment="flex-start">
-						<Text color="gray1" size="small">
-							<a href={roomLink} target="_blank" rel="noreferrer">
-								{roomName}
-							</a>
-						</Text>
+				<Tooltip label={tooltipLabel}>
+					<Row mainAlignment="flex-start">
+						{showIcon && (
+							<Padding right="small">
+								<Icon icon="VideoOutline" size="medium" />
+							</Padding>
+						)}
+						<Row takeAvailableSpace mainAlignment="flex-start">
+							<Text color="gray1" size="small">
+								<a href={roomLink} target="_blank" rel="noreferrer">
+									{roomName}
+								</a>
+							</Text>
+						</Row>
 					</Row>
-				</Row>
+				</Tooltip>
 			</Row>
 		);
 	}
