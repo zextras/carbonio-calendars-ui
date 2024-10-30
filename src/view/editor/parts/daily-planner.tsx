1460f91e3c65090c681091f3874f865762ceda93
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-10-30 10:23:52 +0100

    WIP: daily planner

--- src/view/editor/parts/daily-planner.tsx
+++ src/view/editor/parts/daily-planner.tsx
@@ -6,16 +6,42 @@
 
 import React from 'react';
 
+import { Table, TableBody, TableCell, TableRow } from '@mui/material';
 import { Chip, Container } from '@zextras/carbonio-design-system';
 
 import { useAppSelector } from '../../../store/redux/hooks';
 import { selectSender } from '../../../store/selectors/editor';
 
 export const DailyPlanner = ({ editorId }: { editorId: string }): React.JSX.Element => {
+	const hours = [
+		'12',
+		...Array.from({ length: 11 }, (_, i) => (i + 1).toString()),
+		'12',
+		...Array.from({ length: 11 }, (_, i) => (i + 1).toString())
+	];
 	const sender = useAppSelector(selectSender(editorId));
 	return (
 		<Container data-testid={`daily-planner-component-${editorId}`}>
-			<Chip label={sender.fullName} />
+			<Table>
+				<TableBody>
+					<TableRow>
+						<TableCell style={{ width: '500px' }} />
+						{hours.map((hour, index) => (
+							<TableCell key={index} colSpan={2} style={{ textAlign: 'center' }}>
+								{hour}
+							</TableCell>
+						))}
+					</TableRow>
+					<TableRow>
+						<TableCell style={{ width: '500px', border: '1px solid gray' }}>
+							<Chip label={sender.fullName} />
+						</TableCell>
+						{Array.from({ length: 24 * 2 }, (_, index) => (
+							<TableCell key={index} style={{ width: '500px', border: '1px solid gray' }} />
+						))}
+					</TableRow>
+				</TableBody>
+			</Table>
 		</Container>
 	);
 };
