df6926a45896b32b5e41f5c98ed89ddfed4d3f15
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-10-30 11:48:33 +0100

    chore: remove expand board on button click

--- src/view/editor/editor-panel.tsx
+++ src/view/editor/editor-panel.tsx
@@ -6,7 +6,6 @@
 import React, { ReactElement, useState } from 'react';
 
 import { Button, Container, Divider, Row } from '@zextras/carbonio-design-system';
-import { expandBoards } from '@zextras/carbonio-shell-ui';
 import { isEmpty } from 'lodash';
 import { useTranslation } from 'react-i18next';
 
@@ -54,7 +53,6 @@ export const EditorPanel = ({ editorId, expanded }: EditorProps): ReactElement |
 	const [showDailyPlanner, setShowDailyPlanner] = useState(false);
 	const handleDailyPlannerButtonClick = (): void => {
 		setShowDailyPlanner((state) => !state);
-		expandBoards();
 	};
 	const startDate = useAppSelector(selectEditorStart(editorId));
 	const endDate = useAppSelector(selectEditorEnd(editorId));
@@ -123,7 +121,6 @@ export const EditorPanel = ({ editorId, expanded }: EditorProps): ReactElement |
 				<Row height="fit" width="fill" padding={{ top: 'large' }} mainAlignment="flex-start">
 					<EditorAllDayCheckbox editorId={editorId} />
 				</Row>
-				{/* TODO: add daily planner button */}
 				<Button
 					onClick={handleDailyPlannerButtonClick}
 					label={dailyPlannerLabel}
