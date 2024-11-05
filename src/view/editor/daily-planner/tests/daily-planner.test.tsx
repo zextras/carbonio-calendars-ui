f4af9e2ff9f56710de14e17e1e354b42c80e5008
Davide Frison <davide.frison@zextras.com>
2024-11-05 18:14:24 +0100

    test: add test to daily-planner.test.tsx
    - we must implement the logic of freebusy according to our needs

--- src/view/editor/daily-planner/tests/daily-planner.test.tsx
+++ src/view/editor/daily-planner/tests/daily-planner.test.tsx
@@ -7,11 +7,14 @@
 import React from 'react';
 
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { within } from '@testing-library/react';
 
 import { setupTest, screen } from '../../../../carbonio-ui-commons/test/test-setup';
 import { generateEditor } from '../../../../commons/editor-generator';
 import { reducers } from '../../../../store/redux';
 import mockedData from '../../../../test/generators';
+import { CalendarSender, Resource } from '../../../../types/editor';
+import { EditorChipAttendees } from '../../../../types/store/invite';
 import { EditorDailyPlanner } from '../daily-planner';
 
 const folder = {
@@ -27,13 +30,39 @@ const folders = mockedData.calendars.getCalendarsMap({ folders: [folder] });
 describe('EditorDailyPlanner', () => {
 	it('should render the daily planner component', () => {
 		const store = configureStore({ reducer: combineReducers(reducers) });
-		const context = { folders, dispatch: store.dispatch };
+
+		const organizer: CalendarSender = { address: 'organizer@test.com', fullName: 'Organizer' };
+		const attendees: EditorChipAttendees[] = [
+			{ email: 'attendee1@test.com' },
+			{ email: 'attendee2@test.com' }
+		];
+		const optionalAttendees: EditorChipAttendees[] = [
+			{
+				email: 'optionalAttendee1@test.com'
+			}
+		];
+		const meetingRoom: Resource = { email: 'meeting.room1@test.com', label: 'Meeting Room 1' };
+		const equipment: Resource = { email: 'companyCar@test.com', label: 'Company Car' };
 		const editor = generateEditor({
-			context
+			context: {
+				attendees,
+				optionalAttendees,
+				sender: organizer,
+				meetingRoom: [meetingRoom],
+				equipment: [equipment],
+				folders,
+				dispatch: store.dispatch
+			}
 		});
 
 		setupTest(<EditorDailyPlanner editorId={editor.id} />, { store });
-		expect(screen.getByTestId(`daily-planner-component-${editor.id}`)).toBeInTheDocument();
-		expect(screen.getByText(editor.organizer.fullName)).toBeVisible();
+		const timeTable = screen.getByTestId(`time-table`);
+		expect(timeTable).toBeInTheDocument();
+		expect(within(timeTable).getByText('organizer@test.com')).toBeVisible();
+		expect(within(timeTable).getByText('attendee1@test.com')).toBeVisible();
+		expect(within(timeTable).getByText('attendee2@test.com')).toBeVisible();
+		expect(within(timeTable).getByText('optionalAttendee1@test.com')).toBeVisible();
+		expect(within(timeTable).getByText('companyCar@test.com')).toBeVisible();
+		expect(within(timeTable).getByText('meeting.room1@test.com')).toBeVisible();
 	});
 });
