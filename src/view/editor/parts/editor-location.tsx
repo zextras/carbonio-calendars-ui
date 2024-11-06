a87ef362d3144eb26680af72cc5eb547721310ff
Luca Stauble <luca.stauble@zextras.com>
2024-11-06 17:51:51 +0100

    feat: get location from extensions (#513)
    Refs: CO-1506

--- src/view/editor/parts/editor-location.tsx
+++ src/view/editor/parts/editor-location.tsx
@@ -5,20 +5,38 @@
  */
 import React, { ReactElement, useCallback, useEffect, useMemo, useState } from 'react';
 
-import { Input } from '@zextras/carbonio-design-system';
+import { Button, Container, Input, Row, Tooltip } from '@zextras/carbonio-design-system';
+import { Action, useActions } from '@zextras/carbonio-shell-ui';
 import { isNil, debounce } from 'lodash';
 import { useTranslation } from 'react-i18next';
 
 import { useAppDispatch, useAppSelector } from '../../../store/redux/hooks';
-import { selectEditorDisabled, selectEditorLocation } from '../../../store/selectors/editor';
+import {
+	selectEditorDisabled,
+	selectEditorLocation,
+	selectEditorTitle
+} from '../../../store/selectors/editor';
 import { editEditorLocation } from '../../../store/slices/editor-slice';
+
+interface EditorLocationExtension extends Action {
+	execute: (options: { eventTitle?: string; onConfirm: (newLocation: string) => void }) => void;
+}
 
 export const EditorLocation = ({ editorId }: { editorId: string }): ReactElement | null => {
 	const [t] = useTranslation();
+	const eventTitle = useAppSelector(selectEditorTitle(editorId));
 	const location = useAppSelector(selectEditorLocation(editorId));
 	const [value, setValue] = useState(location ?? '');
 	const disabled = useAppSelector(selectEditorDisabled(editorId));
 	const dispatch = useAppDispatch();
+	const locationExtensions = useActions<undefined, EditorLocationExtension>(
+		undefined,
+		'calendars-location-provider'
+	);
+	const extension = useMemo(
+		() => (locationExtensions.length > 0 ? locationExtensions[0] : null),
+		[locationExtensions]
+	);
 
 	useEffect(() => {
 		if (location) {
@@ -49,14 +67,48 @@ export const EditorLocation = ({ editorId }: { editorId: string }): ReactElement
 		[debounceInput]
 	);
 
+	const onLocationProviderRequest = useCallback(() => {
+		if (!extension) {
+			return;
+		}
+
+		extension.execute({
+			eventTitle,
+			onConfirm: (newLocation: string) => {
+				setValue(newLocation);
+				debounceInput(newLocation);
+			}
+		});
+	}, [debounceInput, eventTitle, extension]);
+
 	return !isNil(location) ? (
-		<Input
-			label={t('label.location', 'Location')}
-			value={value}
-			onChange={onChange}
-			disabled={disabled?.location}
-			backgroundColor="gray5"
-			data-testid="editor-location"
-		/>
+		<Container
+			orientation={'horizontal'}
+			gap={'0.5rem'}
+			width={'fill'}
+			mainAlignment={'flex-start'}
+		>
+			<Input
+				label={t('label.location', 'Location')}
+				value={value}
+				onChange={onChange}
+				disabled={disabled?.location}
+				backgroundColor="gray5"
+				data-testid="editor-location"
+			/>
+
+			{extension && (
+				<Row>
+					<Tooltip label={t('label.location_provider', 'Location Provider')}>
+						<Button
+							type="ghost"
+							size={'extralarge'}
+							icon="PinOutline"
+							onClick={onLocationProviderRequest}
+						/>
+					</Tooltip>
+				</Row>
+			)}
+		</Container>
 	) : null;
 };
