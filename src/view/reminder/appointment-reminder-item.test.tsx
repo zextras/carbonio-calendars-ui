b715cf76dfb6fd5895903dc071648ddc5a3729e5
Luca Stauble <luca.stauble@gmail.com>
2025-01-30 17:12:46 +0100

    test: add new tests

--- src/view/reminder/appointment-reminder-item.test.tsx
+++ src/view/reminder/appointment-reminder-item.test.tsx
@@ -7,29 +7,82 @@ import React from 'react';
 
 import { faker } from '@faker-js/faker';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { screen } from '@testing-library/react';
+import moment from 'moment-timezone';
+import { useTheme } from 'styled-components';
 
 import { AppointmentReminderItem } from './appointment-reminder-item';
-import { setupTest } from '../../carbonio-ui-commons/test/test-setup';
+import { screen, setupHook, setupTest } from '../../carbonio-ui-commons/test/test-setup';
 import { reducers } from '../../store/redux';
 import { generateReminderItem } from '../../test/generators/reminder';
-/* - aggiungere nel reminder di un appuntamento una stringa:
-  - la stringa recita "show event details"
-  - la stringa è underlined
-  - la stringa è blu
-  - la stringa è cliccabile
-- cliccando sulla stringa "show event details" questa cambierà in "hide event details"
-- cliccando sulla stringa "show event details" partirà una richiesta al server
-- se non si hanno ancora i dati da mostrare ci sarà uno shimmer component
-- se si hanno i dati da mostrare allora saranno disponibili
-- se non ci sono dettagli mostrerà una stringa specifica
-- il container dei dati da mostrare ha una max height specifica */
 
 describe('Appointment Reminder Item', () => {
+	it('should render the icon', () => {
+		const reminderItem = generateReminderItem();
+		const store = configureStore({ reducer: combineReducers(reducers) });
+
+		setupTest(
+			<AppointmentReminderItem
+				reminderItem={reminderItem}
+				toggleModal={jest.fn}
+				removeReminder={jest.fn}
+				setActiveReminder={jest.fn}
+			/>,
+			{ store }
+		);
+
+		expect(screen.getByTestId('icon: PhoneCallOutline')).toBeVisible();
+	});
+
+	it('should render the appointment name', () => {
+		const reminderItem = generateReminderItem();
+		const store = configureStore({ reducer: combineReducers(reducers) });
+
+		setupTest(
+			<AppointmentReminderItem
+				reminderItem={reminderItem}
+				toggleModal={jest.fn}
+				removeReminder={jest.fn}
+				setActiveReminder={jest.fn}
+			/>,
+			{ store }
+		);
+
+		expect(screen.getByText(reminderItem.name)).toBeVisible();
+	});
+
+	it('should render the appointment time', () => {
+		const reminderItem = generateReminderItem();
+		const store = configureStore({ reducer: combineReducers(reducers) });
+		const timeText = `${moment(reminderItem.start).format('HH:mm')} - ${moment(reminderItem.end).format('HH:mm')}`;
+
+		setupTest(
+			<AppointmentReminderItem
+				reminderItem={reminderItem}
+				toggleModal={jest.fn}
+				removeReminder={jest.fn}
+				setActiveReminder={jest.fn}
+			/>,
+			{ store }
+		);
+
+		expect(screen.getByText(timeText)).toBeVisible();
+	});
+
+	it.todo('should render the appointment status');
+
+	it.todo("should render a button to snooze the reminder if the appointment isn't started yet");
+
+	it.todo(
+		'should render a button to set a new time for the appointment if the appointment is started'
+	);
+
+	it.todo('should render a button to dismiss the reminder');
+
 	describe('Details expansion link', () => {
 		it('shouldn\'t show a "Show event details" string if the appointment has no details to show', () => {
 			const reminderItem = generateReminderItem({ location: '' });
 			const store = configureStore({ reducer: combineReducers(reducers) });
+
 			setupTest(
 				<AppointmentReminderItem
 					reminderItem={reminderItem}
@@ -39,12 +92,14 @@ describe('Appointment Reminder Item', () => {
 				/>,
 				{ store }
 			);
+
 			expect(screen.queryByText(/show event details/i)).not.toBeInTheDocument();
 		});
 
 		it('should show a "Show event details" string if the appointment has details to show', () => {
-			const reminderItem = generateReminderItem({ location: faker.word.words() });
+			const reminderItem = generateReminderItem({ location: faker.internet.url() });
 			const store = configureStore({ reducer: combineReducers(reducers) });
+
 			setupTest(
 				<AppointmentReminderItem
 					reminderItem={reminderItem}
@@ -54,17 +109,151 @@ describe('Appointment Reminder Item', () => {
 				/>,
 				{ store }
 			);
+
 			expect(screen.getByText(/show event details/i)).toBeVisible();
 		});
 
-		it.todo('should render the text with a specific color');
+		it('should render the text with a specific color', () => {
+			const reminderItem = generateReminderItem({ location: faker.internet.url() });
+			const store = configureStore({ reducer: combineReducers(reducers) });
+			const {
+				result: { current: theme }
+			} = setupHook(useTheme);
 
-		it.todo('should render the text with underline');
+			setupTest(
+				<AppointmentReminderItem
+					reminderItem={reminderItem}
+					toggleModal={jest.fn}
+					removeReminder={jest.fn}
+					setActiveReminder={jest.fn}
+				/>,
+				{ store }
+			);
 
-		it.todo('should render a pointer cursor on hover');
+			const showDetailsText = screen.getByText(/show event details/i);
 
-		it.todo('should change the "Show event details" string to "Hide event details" when clicked');
+			/*
+			 * FIXME change the color variant from hover to regular as soon as
+			 *  the https://github.com/testing-library/jest-dom/issues/594 issue is fixed
+			 */
+			expect(showDetailsText).toHaveStyle({ color: theme.palette.info.hover });
+		});
 
-		it.todo('');
+		it('should render the text with underline', () => {
+			const reminderItem = generateReminderItem({ location: faker.internet.url() });
+			const store = configureStore({ reducer: combineReducers(reducers) });
+
+			setupTest(
+				<AppointmentReminderItem
+					reminderItem={reminderItem}
+					toggleModal={jest.fn}
+					removeReminder={jest.fn}
+					setActiveReminder={jest.fn}
+				/>,
+				{ store }
+			);
+
+			const showDetailsText = screen.getByText(/show event details/i);
+
+			expect(showDetailsText).toHaveStyle({ textDecoration: 'underline' });
+		});
+
+		it('should render a pointer cursor on hover', () => {
+			const reminderItem = generateReminderItem({ location: faker.internet.url() });
+			const store = configureStore({ reducer: combineReducers(reducers) });
+
+			setupTest(
+				<AppointmentReminderItem
+					reminderItem={reminderItem}
+					toggleModal={jest.fn}
+					removeReminder={jest.fn}
+					setActiveReminder={jest.fn}
+				/>,
+				{ store }
+			);
+
+			const showDetailsText = screen.getByText(/show event details/i);
+
+			expect(showDetailsText).toHaveStyle({ cursor: 'pointer' });
+		});
+
+		it('should change the "Show event details" string to "Hide event details" when clicked', async () => {
+			const reminderItem = generateReminderItem({ location: faker.internet.url() });
+			const store = configureStore({ reducer: combineReducers(reducers) });
+
+			const { user } = setupTest(
+				<AppointmentReminderItem
+					reminderItem={reminderItem}
+					toggleModal={jest.fn}
+					removeReminder={jest.fn}
+					setActiveReminder={jest.fn}
+				/>,
+				{ store }
+			);
+
+			await user.click(screen.getByText(/show event details/i));
+
+			expect(screen.getByText(/hide event details/i)).toBeVisible();
+		});
+
+		it('should hide the location URL as default behaviour', async () => {
+			const locationUrl = faker.internet.url();
+			const reminderItem = generateReminderItem({ location: locationUrl });
+			const store = configureStore({ reducer: combineReducers(reducers) });
+
+			const { user } = setupTest(
+				<AppointmentReminderItem
+					reminderItem={reminderItem}
+					toggleModal={jest.fn}
+					removeReminder={jest.fn}
+					setActiveReminder={jest.fn}
+				/>,
+				{ store }
+			);
+
+			expect(screen.queryByText(locationUrl)).not.toBeInTheDocument();
+		});
+
+		it('should set the location URL as visible when the "Show event details" string is clicked', async () => {
+			const locationUrl = faker.internet.url();
+			const reminderItem = generateReminderItem({ location: locationUrl });
+			const store = configureStore({ reducer: combineReducers(reducers) });
+
+			const { user } = setupTest(
+				<AppointmentReminderItem
+					reminderItem={reminderItem}
+					toggleModal={jest.fn}
+					removeReminder={jest.fn}
+					setActiveReminder={jest.fn}
+				/>,
+				{ store }
+			);
+
+			await user.click(screen.getByText(/show event details/i));
+
+			expect(screen.getByText(locationUrl)).toBeVisible();
+		});
+
+		it('should set the location URL as hidden when the "Hide event details" string is clicked', async () => {
+			const locationUrl = faker.internet.url();
+			const reminderItem = generateReminderItem({ location: locationUrl });
+			const store = configureStore({ reducer: combineReducers(reducers) });
+
+			const { user } = setupTest(
+				<AppointmentReminderItem
+					reminderItem={reminderItem}
+					toggleModal={jest.fn}
+					removeReminder={jest.fn}
+					setActiveReminder={jest.fn}
+				/>,
+				{ store }
+			);
+
+			await user.click(screen.getByText(/show event details/i));
+
+			await user.click(screen.getByText(/hide event details/i));
+
+			expect(screen.queryByText(locationUrl)).not.toBeInTheDocument();
+		});
 	});
 });
