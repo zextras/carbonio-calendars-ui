a4e62fecd19460de4736b0c506f828cd90790c0e
biele <gabriele.marino@zextras.com>
2025-02-19 15:44:16 +0100

    wip: saving progress

--- src/view/editor/parts/editor-free-busy-selector.tsx
+++ src/view/editor/parts/editor-free-busy-selector.tsx
@@ -6,35 +6,109 @@
 import React, { ReactElement, useCallback, useEffect, useMemo, useState } from 'react';
 
 import {
-	Container,
+	AnyColor,
+	Icon,
+	type LabelFactoryProps,
 	Padding,
+	Row,
 	Select,
+	type SelectItem,
 	SingleSelectionOnChange,
-	Text
+	useTheme
 } from '@zextras/carbonio-design-system';
-import type { TFunction } from 'i18next';
 import { find } from 'lodash';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 
-import LabelFactory, { Square } from './select-label-factory';
+import { ColorContainer, LabelText, TextUpperCase } from './select-label-factory';
+import { setCalendarColor } from '../../../normalizations/normalizations-utils';
 import { useAppDispatch, useAppSelector } from '../../../store/redux/hooks';
-import { selectEditorDisabled, selectEditorFreeBusy } from '../../../store/selectors/editor';
+import {
+	selectEditorCalendar,
+	selectEditorDisabled,
+	selectEditorFreeBusy
+} from '../../../store/selectors/editor';
 import { editEditorDisplayStatus } from '../../../store/slices/editor-slice';
 import { InviteFreeBusy } from '../../../types/store/invite';
 
-type ItemProps = {
-	label: string;
-	color: string;
-};
+interface CustomSelectItem extends SelectItem {
+	backgroundColor: string;
+	borderColor: string;
+}
 
-const StatusItemComponent = ({ label, color }: ItemProps): ReactElement => (
-	<Container width="fit" mainAlignment="flex-start" orientation="horizontal">
-		<Square $color={color} />
-		<Padding left="small">
-			<Text>{label}</Text>
+export interface CustomLabelFactoryProps extends LabelFactoryProps {
+	selected: CustomSelectItem[];
+}
+
+const FreeBusyIconItem = styled.div<{ color?: AnyColor; border?: AnyColor }>`
+	width: 1rem;
+	height: 1rem;
+	background: ${({ color }): string | undefined => color};
+	border: ${({ border }): string | undefined => `1px solid ${border}`};
+	border-radius: 0.25rem;
+`;
+
+const FreeBusyItemFactory = ({
+	backgroundColor,
+	borderColor,
+	label
+}: {
+	backgroundColor: string | undefined;
+	borderColor: string | undefined;
+	label: string;
+}): React.JSX.Element => (
+	<Row wrap={'nowrap'}>
+		<Padding right="small">
+			<FreeBusyIconItem color={backgroundColor} border={borderColor} />
 		</Padding>
-	</Container>
+		<TextUpperCase>{label}</TextUpperCase>
+	</Row>
 );
+
+const FreeBusyLabelFactory = (item: CustomLabelFactoryProps): React.JSX.Element => {
+	const { selected, label, open, focus, disabled } = item;
+	return (
+		<ColorContainer
+			orientation="horizontal"
+			width="fill"
+			crossAlignment="center"
+			mainAlignment="space-between"
+			borderRadius="half"
+			padding={{
+				all: 'small'
+			}}
+			background={'gray5'}
+			style={{ cursor: disabled ? 'no-drop' : 'pointer' }}
+		>
+			<Row width="100%" takeAvailableSpace mainAlignment="space-between">
+				<Row
+					orientation="vertical"
+					crossAlignment="flex-start"
+					mainAlignment="flex-start"
+					padding={{ left: 'small' }}
+				>
+					<LabelText size="small" disabled={disabled} $showPrimary={open || focus}>
+						{label}
+					</LabelText>
+					{selected?.[0] && (
+						<FreeBusyItemFactory
+							backgroundColor={selected[0].backgroundColor}
+							label={selected[0].label}
+							borderColor={selected[0].borderColor}
+						/>
+					)}
+				</Row>
+			</Row>
+			<Icon
+				size="large"
+				icon={open ? 'ChevronUpOutline' : 'ChevronDownOutline'}
+				disabled={disabled}
+				style={{ alignSelf: 'center' }}
+				color={(disabled && 'text') || ((open || focus) && 'primary') || 'secondary'}
+			/>
+		</ColorContainer>
+	);
+};
 
 const STATUS_VALUES = {
 	FREE: 'F',
@@ -43,40 +117,86 @@ const STATUS_VALUES = {
 	OUT_OF_OFFICE: 'O'
 };
 
-const getStatusItems = (t: TFunction): Array<any> => [
-	{
-		label: t('label.free', 'Free'),
-		value: STATUS_VALUES.FREE,
-		color: '#ffffff',
-		customComponent: <StatusItemComponent label={t('label.free', 'Free')} color="#ffffff" />
-	},
-	{
-		label: t('label.tentative', 'Tentative'),
-		value: STATUS_VALUES.TENTATIVE,
-		color: '#ffc107',
-		customComponent: (
-			<StatusItemComponent label={t('label.tentative', 'Tentative')} color="#ffc107" />
-		)
-	},
-	{
-		label: t('label.busy', 'Busy'),
-		value: STATUS_VALUES.BUSY,
-		color: '#d5e3f6',
-		customComponent: <StatusItemComponent label={t('label.busy', 'Busy')} color="#d5e3f6" />
-	},
-	{
-		label: t('label.out_of_office', 'Out of office'),
-		value: STATUS_VALUES.OUT_OF_OFFICE,
-		color: '#d5e3f6',
-		customComponent: (
-			<StatusItemComponent label={t('label.out_of_office', 'Out of office')} color="#2b73d2" />
-		)
-	}
-];
+const useGetStatusItems = (editorId: string): Array<any> => {
+	const [t] = useTranslation();
+	const calendar = useAppSelector(selectEditorCalendar(editorId));
+	const calendarColor = setCalendarColor({ color: calendar.color }).color;
+	const backgroundColor = setCalendarColor({ color: calendar.color }).background;
+	const theme = useTheme();
+	const tentativeBackgroundGradient = `repeating-linear-gradient(45deg,
+				${calendarColor},
+				${calendarColor} 3px,
+				${backgroundColor},
+				${backgroundColor} 5px)`;
+
+	return useMemo(
+		() => [
+			{
+				label: t('label.free', 'Free'),
+				value: STATUS_VALUES.FREE,
+				backgroundColor: theme.palette.white.regular,
+				borderColor: calendarColor,
+				customComponent: (
+					<FreeBusyItemFactory
+						backgroundColor={theme.palette.white.regular}
+						borderColor={calendarColor}
+						label={t('label.free', 'Free')}
+					/>
+				)
+			},
+			{
+				label: t('label.tentative', 'Tentative'),
+				value: STATUS_VALUES.TENTATIVE,
+				backgroundColor: tentativeBackgroundGradient,
+				borderColor: calendarColor,
+				customComponent: (
+					<FreeBusyItemFactory
+						backgroundColor={tentativeBackgroundGradient}
+						borderColor={calendarColor}
+						label={t('label.tentative', 'Tentative')}
+					/>
+				)
+			},
+			{
+				label: t('label.busy', 'Busy'),
+				value: STATUS_VALUES.BUSY,
+				backgroundColor: calendarColor,
+				borderColor: calendarColor,
+				customComponent: (
+					<FreeBusyItemFactory
+						backgroundColor={calendarColor}
+						borderColor={calendarColor}
+						label={t('label.busy', 'Busy')}
+					/>
+				)
+			},
+			{
+				label: t('label.out_of_office', 'Out of office'),
+				value: STATUS_VALUES.OUT_OF_OFFICE,
+				borderColor: calendarColor,
+				backgroundColor: theme.palette.gray2.regular,
+				customComponent: (
+					<FreeBusyItemFactory
+						backgroundColor={theme.palette.gray2.regular}
+						borderColor={calendarColor}
+						label={t('label.out_of_office', 'Out of office')}
+					/>
+				)
+			}
+		],
+		[
+			calendarColor,
+			t,
+			tentativeBackgroundGradient,
+			theme.palette.gray2.regular,
+			theme.palette.white.regular
+		]
+	);
+};
 
 export const EditorFreeBusySelector = ({ editorId }: { editorId: string }): ReactElement | null => {
 	const [t] = useTranslation();
-	const statusItems = useMemo(() => getStatusItems(t), [t]);
+	const statusItems = useGetStatusItems(editorId);
 	const freeBusy = useAppSelector(selectEditorFreeBusy(editorId));
 	const disabled = useAppSelector(selectEditorDisabled(editorId));
 	const dispatch = useAppDispatch();
@@ -112,7 +232,7 @@ export const EditorFreeBusySelector = ({ editorId }: { editorId: string }): Reac
 			onChange={onChange}
 			selection={selected}
 			disabled={disabled?.freeBusy}
-			LabelFactory={LabelFactory}
+			LabelFactory={FreeBusyLabelFactory}
 		/>
 	) : null;
 };
