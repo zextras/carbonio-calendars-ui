0e6ccd8864dfb509549fcc2fa439aa3e5629676d
biele <gabriele.marino@zextras.com>
2025-02-20 11:08:55 +0100

    feat: implement free-busy-status-row component

---
+++ src/view/event-summary-view/free-busy-status-row.tsx
@@ -0,0 +1,97 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React, { useMemo } from 'react';
+
+import { Container, Padding, Row, Text, Theme } from '@zextras/carbonio-design-system';
+import { useUserAccount } from '@zextras/carbonio-shell-ui';
+import { Trans, useTranslation } from 'react-i18next';
+
+import { EventType } from '../../types/event';
+import { Invite } from '../../types/store/invite';
+
+export const FreeBusyStatusRowComponent = ({
+	event,
+	invite,
+	fontSize = 'small'
+}: {
+	event: EventType;
+	invite: Invite;
+	fontSize?: keyof typeof Theme.sizes.font;
+}): React.JSX.Element => {
+	const account = useUserAccount();
+	const [t] = useTranslation();
+	const whoSetThis = useMemo(
+		() =>
+			invite?.organizer?.a === account.name
+				? t('message.you', 'You')
+				: t('message.the_organizer', 'The organizer'),
+		[account.name, invite?.organizer?.a, t]
+	);
+
+	const status = useMemo(() => {
+		if (event.resource.freeBusy === 'F') {
+			return t('label.free', 'Free');
+		}
+		if (event.resource.freeBusy === 'T') {
+			return t('label.tentative', 'Tentative');
+		}
+		if (event.resource.freeBusy === 'O') {
+			return t('label.out_of_office', 'Out of office');
+		}
+		return t('label.busy', 'Busy');
+	}, [event.resource.freeBusy, t]);
+	return (
+		<Trans
+			i18nKey="message.the_organizer_set_this"
+			defaults="<Row><Text> <strong> {{whoSetThis}}  </strong> set this appointment as {{status}} </Text></Row>"
+			components={{
+				Row: <Row />,
+				Text: <Text color="secondary" size={fontSize} />
+			}}
+			values={{ whoSetThis, status }}
+		/>
+	);
+};
+
+export const FreeBusyStatusRow = ({
+	event,
+	invite,
+	isSummary,
+	fontSize = 'small'
+}: {
+	event: EventType;
+	invite: Invite;
+	isSummary?: boolean;
+	fontSize?: keyof typeof Theme.sizes.font;
+}): React.JSX.Element => {
+	const padding = isSummary ? '1.5rem' : '1rem';
+
+	return (
+		<Container
+			orientation="vertical"
+			mainAlignment="flex-start"
+			crossAlignment="flex-start"
+			width="fill"
+			height="fit"
+			padding={{ top: 'small' }}
+		>
+			<Container
+				orientation="vertical"
+				mainAlignment="flex-start"
+				crossAlignment="flex-start"
+				width="fill"
+				height="fit"
+				padding={isSummary ? { vertical: 'small' } : { horizontal: 'large', vertical: 'medium' }}
+				background={'gray6'}
+			>
+				<Row mainAlignment="flex-start" crossAlignment="center" width="fill">
+					<Padding left={padding} />
+					<FreeBusyStatusRowComponent invite={invite} fontSize={fontSize} event={event} />
+				</Row>
+			</Container>
+		</Container>
+	);
+};
