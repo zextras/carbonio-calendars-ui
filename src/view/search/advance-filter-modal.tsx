d7441400cab9f33d8b29ee5697309e72aa093cc3
Yuliya <yuliya.aheeva@mail.ru>
2025-05-09 17:58:30 +0200

    fix: reset filters button

--- src/view/search/advance-filter-modal.tsx
+++ src/view/search/advance-filter-modal.tsx
@@ -6,15 +6,21 @@
  */
 import React, { FC, ReactElement, useState, useCallback, useMemo, useEffect } from 'react';
 
-import { CustomModal, Container, ChipProps } from '@zextras/carbonio-design-system';
+import {
+	CustomModal,
+	Container,
+	ChipProps,
+	ModalHeader,
+	Divider,
+	ModalFooter
+} from '@zextras/carbonio-design-system';
 import type { QueryChip } from '@zextras/carbonio-search-ui';
 import { t } from '@zextras/carbonio-shell-ui';
-import { concat, filter, includes, map } from 'lodash';
+import { concat, filter, map } from 'lodash';
 
 import FromDateToDateRow from './parts/from-date-to-date-row';
 import KeywordRow from './parts/keyword-row';
-import ModalFooter from '../../commons/modal-footer';
-import { ModalHeader } from '../../commons/modal-header';
+
 import { DEFAULT_DATE_START, DEFAULT_DATE_END } from '../../constants/advance-filter-modal';
 
 type KeywordState = Array<{
@@ -34,8 +40,8 @@ type AdvancedFilterModalProps = {
 	open: boolean;
 	dateStart: number;
 	dateEnd: number;
-    setDateStart: (arg: number) => void;
-    setDateEnd: (arg: number) => void;
+	setDateStart: (arg: number) => void;
+	setDateEnd: (arg: number) => void;
 	onClose: () => void;
 	query: Array<{
 		id: string;
@@ -54,81 +60,77 @@ const AdvancedFilterModal: FC<AdvancedFilterModalProps> = ({
 	updateQuery,
 	dateStart,
 	dateEnd,
-    setDateStart,
-    setDateEnd
+	setDateStart,
+	setDateEnd
 }): ReactElement => {
 	const [otherKeywords, setOtherKeywords] = useState<KeywordState>([]);
-
-	const [folder, setFolder] = useState<KeywordState>([]);
 	const [fromDate, setFromDate] = useState<any>(dateStart);
 	const [toDate, setToDate] = useState<any>(dateEnd);
-	const queryArray = useMemo(() => ['has:attachment', 'is:flagged', 'is:unread'], []);
 
 	useEffect(() => {
+		if (!open) return;
+		
 		const updatedQuery = map(
 			filter(
 				query,
-				(v) =>
-					!includes(queryArray, v.label) &&
-					!/^Is:/.test(v.label) &&
-					!v.isQueryFilter
+				(v) => !v.isQueryFilter
 			),
 			(q) => ({ ...q, hasAvatar: false })
 		);
 		setOtherKeywords(updatedQuery);
-	}, [query, queryArray]);
 
-
+	}, [query, open]);
 
 	const resetFilters = useCallback(() => {
-        setFromDate(DEFAULT_DATE_START);
-        setDateStart(DEFAULT_DATE_START);
-        setToDate(DEFAULT_DATE_END);
-        setDateEnd(DEFAULT_DATE_END);
-        updateQuery([]);
-		setFolder([]);
+		setFromDate(DEFAULT_DATE_START);
+		setDateStart(DEFAULT_DATE_START);
+		setToDate(DEFAULT_DATE_END);
+		setDateEnd(DEFAULT_DATE_END);
+		updateQuery([]);
 	}, [setDateEnd, setDateStart, updateQuery]);
 
 	const queryToBe = useMemo<Array<QueryChip>>(
-		() => concat(otherKeywords, folder),
-		[folder, otherKeywords]
+		() => concat(otherKeywords),
+		[otherKeywords]
+	);
+
+	const secondaryDisabled = useMemo(
+		() => queryToBe.length === 0 && fromDate === DEFAULT_DATE_START && toDate === DEFAULT_DATE_END,
+		[queryToBe.length, fromDate, toDate, DEFAULT_DATE_START, DEFAULT_DATE_END]
 	);
 
 	const onConfirm = useCallback(() => {
 		updateQuery(queryToBe);
-        setDateStart(fromDate.valueOf());
-        setDateEnd(toDate.valueOf());
+		setDateStart(fromDate.valueOf());
+		setDateEnd(toDate.valueOf());
 		onClose();
 	}, [updateQuery, queryToBe, setDateStart, fromDate, setDateEnd, toDate, onClose]);
 
-
 	return (
 		<CustomModal open={open} onClose={onClose} maxHeight="90vh" size="medium">
-			<Container padding={{ bottom: 'medium' }}>
-				<ModalHeader
-					onClose={onClose}
-					title={t('label.single_advanced_filter', 'Advanced Filters')}
-				/>
-
-				<Container padding={{ horizontal: 'medium', vertical: 'small' }}>
-					<KeywordRow otherKeywords={otherKeywords} setOtherKeywords={setOtherKeywords} />
-					<FromDateToDateRow
-						fromDate={fromDate}
-						setFromDate={setFromDate}
-						toDate={toDate}
-						setToDate={setToDate}
-					/>
-				</Container>
-				<ModalFooter
-					onConfirm={onConfirm}
-					label={t('action.search', 'Search')}
-					secondaryLabel={t('action.reset', 'Reset')}
-					secondaryAction={resetFilters}
-					secondaryBtnType="outlined"
-					secondaryColor="primary"
-					paddingTop="small"
+			<ModalHeader
+				onClose={onClose}
+				title={t('label.single_advanced_filter', 'Advanced Filters')}
+				showCloseIcon
+			/>
+			<Divider />
+			<Container padding={{ horizontal: 'medium', vertical: 'small' }}>
+				<KeywordRow otherKeywords={otherKeywords} setOtherKeywords={setOtherKeywords} />
+				<FromDateToDateRow
+					fromDate={fromDate}
+					setFromDate={setFromDate}
+					toDate={toDate}
+					setToDate={setToDate}
 				/>
 			</Container>
+			<Divider />
+			<ModalFooter
+				onConfirm={onConfirm}
+				confirmLabel={t('action.search', 'Search')}
+				secondaryActionLabel={t('action.reset', 'Reset filters')}
+				onSecondaryAction={resetFilters}
+				secondaryActionDisabled={secondaryDisabled}
+			/>
 		</CustomModal>
 	);
 };
