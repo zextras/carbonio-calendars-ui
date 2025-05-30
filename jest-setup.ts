fa241dc4a94951d0077988cd84d5669f07362f6d
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-05-30 17:23:11 +0200

    refactor(jest-setup.ts): inline failOnConsole config and update mocks
    - Remove unused imports and replace local test utility imports with
      @test-utils equivalents.
    - Inline failOnConsole configuration instead of using getFailOnConsoleDefaultConfig.
    - Add and update mocks for IntersectionObserver, matchMedia, window.open,
      window.crypto.randomUUID, Worker, and ResizeObserver.
    - Refactor test lifecycle hooks to use new server setup and clear timers.

--- jest-setup.ts
+++ jest-setup.ts
@@ -3,21 +3,13 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import '@testing-library/jest-dom';
 import { configure } from '@testing-library/react';
 import failOnConsole from 'jest-fail-on-console';
+import { noop } from 'lodash';
 import moment from 'moment-timezone';
 import { http } from 'msw';
+import { setupServer, SetupServer } from 'msw/node';
 
-import {
-	defaultAfterAllTests,
-	defaultAfterEachTest,
-	defaultBeforeAllTests,
-	defaultBeforeEachTest,
-	getFailOnConsoleDefaultConfig
-} from './src/carbonio-ui-commons/test/jest-setup';
-import { handleGetShareInfoRequest } from './src/carbonio-ui-commons/test/mocks/network/msw/handle-get-share-info';
-import { registerRestHandler } from './src/carbonio-ui-commons/test/mocks/network/msw/handlers';
 import { JEST_DEFAULT_TIMEZONE, JEST_SYSTEM_TIME_DATE } from './src/constants/test-environment';
 import { handleAutoCompleteGalRequest } from './src/test/mocks/network/msw/handle-autocomplete-gal-request';
 import { handleCancelAppointmentRequest } from './src/test/mocks/network/msw/handle-cancel-appointment';
@@ -35,6 +27,67 @@ import { handleSearchCalendarResourcesRequest } from './src/test/mocks/network/m
 import { handleSearchRequest } from './src/test/mocks/network/msw/handle-search-request';
 import { handleSendInviteReplyRequest } from './src/test/mocks/network/msw/handle-send-invite-reply';
 import { handleSendShareNotificationRequest } from './src/test/mocks/network/msw/handle-send-share-notification';
+import { useLocalStorage } from '@test-utils/carbonio-shell-ui/carbonio-shell-ui';
+import { handleGetShareInfoRequest } from '@test-utils/network/msw/handle-get-share-info';
+import { getRestHandlers, registerRestHandler } from '@test-utils/network/msw/handlers';
+
+let server: SetupServer;
+
+/**
+ * Default logic to execute before all the tests
+ */
+type DefaultBeforeAllTestsProps = {
+	onUnhandledRequest: 'warn' | 'error';
+};
+
+export const defaultBeforeAllTests = (
+	{ onUnhandledRequest }: DefaultBeforeAllTestsProps = { onUnhandledRequest: 'warn' }
+): void => {
+	// Do not useFakeTimers with `whatwg-fetch` if using mocked server
+	// https://github.com/mswjs/msw/issues/448
+
+	// mock a simplified Intersection Observer
+	Object.defineProperty(window, 'IntersectionObserver', {
+		writable: true,
+		value: jest.fn(function intersectionObserverMock(
+			callback: IntersectionObserverCallback,
+			options: IntersectionObserverInit
+		) {
+			return {
+				thresholds: options.threshold,
+				root: options.root,
+				rootMargin: options.rootMargin,
+				observe: jest.fn(),
+				unobserve: jest.fn(),
+				disconnect: jest.fn()
+			};
+		})
+	});
+
+	server?.close();
+
+	server = setupServer(...getRestHandlers());
+
+	server.listen({ onUnhandledRequest });
+};
+
+beforeAll(() => {
+	defaultBeforeAllTests();
+	useLocalStorage.mockReturnValue([jest.fn(), jest.fn()]);
+});
+
+beforeEach(noop);
+
+afterEach(() => {
+	jest.clearAllTimers();
+});
+
+afterAll(() => {
+	server.resetHandlers();
+	server.close();
+});
+
+export const getSetupServer = (): SetupServer => server;
 
 global.Notification = jest.fn() as unknown as jest.Mocked<typeof Notification>;
 global.Audio = jest.fn().mockImplementation(() => ({
@@ -45,7 +98,10 @@ configure({
 	asyncUtilTimeout: 2000
 });
 
-failOnConsole({ ...getFailOnConsoleDefaultConfig() });
+failOnConsole({
+	shouldFailOnError: true,
+	shouldFailOnWarn: true
+});
 
 beforeAll(() => {
 	const h = [
@@ -84,16 +140,74 @@ beforeEach(() => {
 		timeZone: JEST_DEFAULT_TIMEZONE
 	});
 	jest.setSystemTime(new Date(JEST_SYSTEM_TIME_DATE));
-	defaultBeforeEachTest();
 });
 
 afterEach(() => {
-	defaultAfterEachTest();
+	jest.clearAllTimers();
 });
 
 afterAll(() => {
-	defaultAfterAllTests();
+	server.resetHandlers();
+	server.close();
 });
+// Mock matchMedia
+// see: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+	writable: true,
+	value: jest.fn().mockImplementation((query) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(), // deprecated
+		removeListener: jest.fn(), // deprecated
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn()
+	}))
+});
+
+// Mock implementation of window.open
+Object.defineProperty(window, 'open', {
+	writable: true,
+	value: jest.fn()
+});
+
+// mock a simplified crypto
+Object.defineProperty(window.crypto, 'randomUUID', {
+	writable: true,
+	value: jest.fn(() => Math.random().toString())
+});
+
+/**
+ * Mocks the Worker class
+ */
+
+type MessageHandler = (msg: string) => void;
+
+class Worker {
+	url: string;
+
+	onmessage: MessageHandler;
+
+	constructor(stringUrl: string) {
+		this.url = stringUrl;
+		this.onmessage = noop;
+	}
+
+	postMessage(msg: string): void {
+		this.onmessage(msg);
+	}
+}
+
+Object.defineProperty(window, 'Worker', {
+	writable: true,
+	value: Worker
+});
+
+window.ResizeObserver = jest.fn().mockImplementation(() => ({
+	observe: jest.fn(),
+	unobserve: jest.fn(),
+	disconnect: jest.fn()
+}));
 
 // mock a simplified crypto
 Object.defineProperty(window.crypto, 'randomUUID', {
