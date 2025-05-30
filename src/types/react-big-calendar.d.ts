e6ea76f2abee18c4c0ae20e3e3a69d3e1d69a85a
Keshav Bhatt <keshavnrj@gmail.com>
2025-05-30 22:03:32 +0530

    refactor: update module imports for react-big-calendar types

--- src/types/react-big-calendar.d.ts
+++ src/types/react-big-calendar.d.ts
@@ -4,9 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import type * as ReactBigCalendar from 'react-big-calendar';
-
-declare module 'react-big-calendar' {
+declare module './react-big-calendar' {
 	interface TimeGridProps extends ReactBigCalendar.TimeGridProps {
 		enableAutoScroll?: boolean;
 		eventOffset?: ReactBigCalendar.TimeGridProps['eventOffset'];
