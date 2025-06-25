8b403308e1a13f70049e00e61a8bb64b228aaaf9
Keshav Bhatt <keshavnrj@gmail.com>
2025-06-25 18:47:47 +0530

    refactor: remove deprecated searchCalendarResourcesRequest function

--- src/soap/search-calendar-resources-request.ts
+++ src/soap/search-calendar-resources-request.ts
@@ -28,26 +28,6 @@ export type searchCalendarFulfilledType = {
 
 export type searchCalendarReturnType = searchCalendarFulfilledType | searchCalendarRejectedType;
 
-export const searchCalendarResourcesRequest = async (
-	value: string
-): Promise<searchCalendarReturnType> => {
-	const response: searchCalendarReturnType = await soapFetch('SearchCalendarResources', {
-		attrs: `${SEARCH_RESOURCES_ATTRS.EMAIL},${SEARCH_RESOURCES_ATTRS.CAL_RES_TYPE},${SEARCH_RESOURCES_ATTRS.FULL_NAME}`,
-		searchFilter: {
-			conds: {
-				or: '1',
-				cond: {
-					attr: SEARCH_RESOURCES_ATTRS.CAL_RES_TYPE,
-					op: SEARCH_RESOURCE_OP.EQUAL,
-					value
-				}
-			}
-		},
-		_jsns: 'urn:zimbraAccount'
-	});
-	return response?.Fault ? { ...response.Fault, error: true } : response;
-};
-
 export const searchCalendarMultipleResourcesRequest = async (
 	values: Array<string>
 ): Promise<searchCalendarReturnType> => {
