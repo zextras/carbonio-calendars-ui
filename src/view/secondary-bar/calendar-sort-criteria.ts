8929306063239d9bf01d3146631735a96ab1b742
Luca Stauble <luca.stauble@gmail.com>
2025-09-04 19:41:52 +0200

    refactor(SecondaryBar): add stub for sort algorithm

---
+++ src/view/secondary-bar/calendar-sort-criteria.ts
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+export const getCalendarSortCriteria = (calendar: Folder): string => {
+	// const { id, zid } = getFolderIdParts(folder.id);
+	// if (isRoot(folder.id)) {
+	// 	return zid === null ? `0100` : `0500-${folder.name.toLowerCase()}`;
+	// }
+	// if (isLink(folder)) {
+	// 	return `5000-${folder.name.toLowerCase()}`;
+	// }
+	// switch (id) {
+	// 	case FOLDERS.CONTACTS:
+	// 		return `1000`;
+	// 	case FOLDERS.AUTO_CONTACTS:
+	// 		return `2000`;
+	// 	case FOLDERS.TRASH:
+	// 		return `4000`;
+	// 	default:
+	// 		return `3000-${folder.name.toLowerCase()}`;
+	// }
+};
