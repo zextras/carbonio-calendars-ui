bd9406428f32978d0f547d1fdf3d445b6f885cd6
Luca Stauble <luca.stauble@gmail.com>
2025-09-04 19:47:13 +0200

    refactor(SecondaryBar): optimize hook

--- src/view/secondary-bar/use-secondary-bar-tree-groups.ts
+++ src/view/secondary-bar/use-secondary-bar-tree-groups.ts
@@ -3,35 +3,41 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import { useMemo } from 'react';
+
 import { AccordionItemType } from '@zextras/carbonio-design-system';
-import { compact, find, map, reject, sortBy } from 'lodash';
+import { compact, map, reject, sortBy } from 'lodash';
 import { useTranslation } from 'react-i18next';
 
+import { GroupAccordionItem } from './custom-accordion-components/group-accordion-item';
 import { SIDEBAR_ITEMS } from '../../constants/sidebar';
 import { useCalendarGroups } from '../../store/zustand/calendar-group-store';
-import { GroupAccordionItem } from './custom-accordion-components/group-accordion-item';
 
 export const useSecondaryBarTreeGroups = (): Array<AccordionItemType> => {
 	const [t] = useTranslation();
 	const groups = useCalendarGroups();
 
-	const allCalendars = find(groups, ['id', SIDEBAR_ITEMS.ALL_CALENDAR]);
-	const otherGroups = reject(groups, ['id', SIDEBAR_ITEMS.ALL_CALENDAR]);
-	const sortedGroups = compact([
-		allCalendars,
-		...sortBy(otherGroups, (group) => group.name.toLowerCase())
-	]);
+	const allCalendars = useMemo(() => groups[SIDEBAR_ITEMS.ALL_CALENDAR], [groups]);
+	const otherGroups = useMemo(() => reject(groups, ['id', SIDEBAR_ITEMS.ALL_CALENDAR]), [groups]);
+	const sortedGroups = useMemo(
+		() => compact([allCalendars, ...sortBy(otherGroups, (group) => group.name.toLowerCase())]),
+		[allCalendars, otherGroups]
+	);
 
-	return map(sortedGroups, (group) => {
-		const label =
-			group.id === SIDEBAR_ITEMS.ALL_CALENDAR
-				? t('label.all_calendars', 'All calendars')
-				: group.name;
+	return useMemo(
+		() =>
+			map(sortedGroups, (group) => {
+				const label =
+					group.id === SIDEBAR_ITEMS.ALL_CALENDAR
+						? t('label.all_calendars', 'All calendars')
+						: group.name;
 
-		return {
-			id: group.id,
-			label,
-			CustomComponent: GroupAccordionItem
-		};
-	});
+				return {
+					id: group.id,
+					label,
+					CustomComponent: GroupAccordionItem
+				};
+			}),
+		[sortedGroups, t]
+	);
 };
