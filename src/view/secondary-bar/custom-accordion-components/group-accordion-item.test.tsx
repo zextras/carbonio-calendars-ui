285770d07258cf99e0d55e217a5868395abf4442
Luca Stauble <luca.stauble@gmail.com>
2025-09-04 08:35:01 +0200

    refactor(SecondaryBar): add accordion calendar groups items custom component

---
+++ src/view/secondary-bar/custom-accordion-components/group-accordion-item.test.tsx
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+describe('GroupAccordionItem', () => {
+	it.todo('should display the correct icon');
+
+	it.todo('should display the icon disabled if the group is not active');
+
+	it.todo('should display the icon enabled if the group is active');
+
+	it.todo('should display the correct label');
+
+	it.todo('should display a warning icon if the group has no calendars');
+
+	it.todo('should call the onClick callback when clicked');
+});
