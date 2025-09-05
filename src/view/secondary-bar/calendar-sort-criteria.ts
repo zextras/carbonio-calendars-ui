1f7b84c6533d64d7842e969e29b95e090e25ade5
Luca Stauble <luca.stauble@gmail.com>
2025-09-05 17:52:23 +0200

    refactor(SecondaryBar): implement calendars sorting algorithm

--- src/view/secondary-bar/calendar-sort-criteria.ts
+++ src/view/secondary-bar/calendar-sort-criteria.ts
@@ -3,22 +3,22 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import { Folder, FOLDERS, getFolderIdParts, isLink, isRoot } from '@zextras/carbonio-ui-commons';
+
 export const getCalendarSortCriteria = (calendar: Folder): string => {
-	// const { id, zid } = getFolderIdParts(folder.id);
-	// if (isRoot(folder.id)) {
-	// 	return zid === null ? `0100` : `0500-${folder.name.toLowerCase()}`;
-	// }
-	// if (isLink(folder)) {
-	// 	return `5000-${folder.name.toLowerCase()}`;
-	// }
-	// switch (id) {
-	// 	case FOLDERS.CONTACTS:
-	// 		return `1000`;
-	// 	case FOLDERS.AUTO_CONTACTS:
-	// 		return `2000`;
-	// 	case FOLDERS.TRASH:
-	// 		return `4000`;
-	// 	default:
-	// 		return `3000-${folder.name.toLowerCase()}`;
-	// }
+	const { id, zid } = getFolderIdParts(calendar.id);
+	if (isRoot(calendar.id)) {
+		return zid === null ? `0100` : `0500-${calendar.name.toLowerCase()}`;
+	}
+	if (isLink(calendar)) {
+		return `5000-${calendar.name.toLowerCase()}`;
+	}
+	switch (id) {
+		case FOLDERS.CALENDAR:
+			return `1000`;
+		case FOLDERS.TRASH:
+			return `2000`;
+		default:
+			return `3000-${calendar.name.toLowerCase()}`;
+	}
 };
