012c9a0aeaa1aca21993c7cc51bf144f82c6cd37
Dennis <dennis.ceron@zextras.com>
2025-09-11 13:04:45 +0200

    test: fix test usesecondarybartreeprimaryaccount

--- src/view/secondary-bar/use-secondary-bar-tree-primary-account.test.ts
+++ src/view/secondary-bar/use-secondary-bar-tree-primary-account.test.ts
@@ -8,13 +8,17 @@ import { getRootsArray } from '@zextras/carbonio-ui-commons';
 
 import { AccountAccordionItem } from './custom-accordion-components/account-accordion-item';
 import { useSecondaryBarTreePrimaryAccount } from './use-secondary-bar-tree-primary-account';
+import { useLocalStorage } from '../../../__mocks__/@zextras/carbonio-shell-ui';
 import { populateFoldersStore } from '../../__test__/mocks/store/folders';
 import { setupHook } from '../../__test__/test-setup';
 import { SIDEBAR_ROOT_SUBSECTION } from '../../constants/sidebar';
 import { generateGroupCalendar, populateGroupsStore } from '../../test/generators/group';
 
-describe('useSecondaryBarTreeAccounts', () => {
+describe('useSecondaryBarTreePrimaryAccount', () => {
 	it('should return an object with the correct structure', () => {
+		// Mock the useLocalStorage hook to simulate localStorage containing the id '1'
+		useLocalStorage.mockReturnValue([[], jest.fn()]);
+
 		populateFoldersStore({ view: 'appointment' });
 		const groups = [generateGroupCalendar()];
 		populateGroupsStore({ groups });
@@ -32,14 +36,33 @@ describe('useSecondaryBarTreeAccounts', () => {
 			label: account?.name,
 			CustomComponent: AccountAccordionItem,
 			items: [
-				{ id: SIDEBAR_ROOT_SUBSECTION.CALENDARS, label: 'Calendars', items: expect.any(Array) },
-				{ id: SIDEBAR_ROOT_SUBSECTION.GROUPS, label: 'Groups', items: expect.any(Array) }
-			]
+				{
+					id: SIDEBAR_ROOT_SUBSECTION.CALENDARS,
+					label: 'Calendars',
+					items: expect.any(Array),
+					onClose: expect.any(Function),
+					onOpen: expect.any(Function),
+					open: false
+				},
+				{
+					id: SIDEBAR_ROOT_SUBSECTION.GROUPS,
+					label: 'Groups',
+					items: expect.any(Array),
+					onClose: expect.any(Function),
+					onOpen: expect.any(Function),
+					open: false
+				}
+			],
+			onClose: expect.any(Function),
+			onOpen: expect.any(Function),
+			open: false
 		};
 
+		const calendarsItemsAndFindShares = accountRoots[0].children.length + 1; // Assuming folders are directly under the primary account
+		const groupsAndAllCalendarsAndAddGroups = groups.length + 2; // +2 for "All Calendars" group and "Add Group" button
 		// Assert the length of items in Calendars and Groups
-		expect(result?.items?.[0].items).toHaveLength(accountRoots[0].children.length); // Assuming folders are directly under the primary account
-		expect(result?.items?.[1].items).toHaveLength(groups.length + 1); // +1 for "All Calendars" group
+		expect(result?.items?.[0].items).toHaveLength(calendarsItemsAndFindShares);
+		expect(result?.items?.[1].items).toHaveLength(groupsAndAllCalendarsAndAddGroups);
 
 		// Assert the overall structure
 		expect(result).toEqual(expectedResult);
