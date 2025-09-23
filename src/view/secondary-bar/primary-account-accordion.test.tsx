c748cacf19431d44b8fe693905f53e563db43afd
Dennis <dennis.ceron@zextras.com>
2025-09-23 14:52:55 +0200

    test: primaryaccoutaccordion test

--- src/view/secondary-bar/primary-account-accordion.test.tsx
+++ src/view/secondary-bar/primary-account-accordion.test.tsx
@@ -3,3 +3,28 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import React from 'react';
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+import { PrimaryAccountAccordion } from './primary-account-accordion';
+import { useLocalStorage } from '../../../__mocks__/@zextras/carbonio-shell-ui';
+import { setupTest, screen } from '@test-setup';
+import { populateFoldersStore } from '@test-utils/store/folders';
+import { getMocksContext } from '@test-utils/utils/mocks-context';
+import { reducers } from 'store/redux';
+
+describe('PrimaryAccountAccordion', () => {
+	it('should render the primary account email', () => {
+		useLocalStorage.mockReturnValue([[], jest.fn()]);
+		populateFoldersStore();
+		const mockedContext = getMocksContext();
+		const store = configureStore({
+			reducer: combineReducers(reducers)
+		});
+
+		setupTest(<PrimaryAccountAccordion />, { store });
+
+		expect(screen.getByText(mockedContext.identities.primary.identity.email)).toBeVisible();
+	});
+});
