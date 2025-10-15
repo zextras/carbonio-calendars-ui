c3825027222c93bdab809dc06d661c47e6125324
Keshav Bhatt <keshavnrj@gmail.com>
2025-10-15 15:09:54 +0530

    feat: add path mappings for test mocks in configuration files

--- jest.config.ts
+++ jest.config.ts
@@ -38,6 +38,7 @@ const config: Config = {
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/__mocks__/fileMock.js',
 		'^uuid$': require.resolve('uuid'),
+		'^@test-mocks/(.*)$': '<rootDir>/__mocks__/$1',
 		'^@test-utils/(.*)$': '<rootDir>/src/__test__/mocks/$1',
 		'^@test-setup$': '<rootDir>/src/__test__/test-setup.tsx',
 		'^@jest-setup$': '<rootDir>/jest-setup.ts'
