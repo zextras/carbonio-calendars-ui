9fc42ce3ba522158faa3020585151c31a6a2bd93
Davide Frison <davide.frison@zextras.com>
2025-10-24 18:11:24 +0200

    test: do not include integration tests file in coverage pt.2

--- jest.config.ts
+++ jest.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
 	collectCoverageFrom: [
 		'src/**/*.{js,ts}(x)?',
 		'!**/__mocks__/**', // Exclude mock files
-		'!**/integration-tests/**', // Exclude integration tests
+		'!src/integration-tests/**', // Exclude integration tests
 		'!**/__tests__/**', // Exclude test files
 		'!**/*.test.{js,jsx,ts,tsx}', // Exclude test files
 		'!**/*.spec.{js,jsx,ts,tsx}', // Exclude test files
