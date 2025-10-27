479ae84f869502f3f5c56f0da44a1f7cf61411b8
Davide Frison <davide.frison@zextras.com>
2025-10-27 10:18:04 +0100

    test: add test for calendar restore

---
+++ src/__test__/mocks/api/restore-calendar.ts
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { FolderActionRequest, FolderActionResponseOk } from '../../../types/soap/soap-actions';
+import { createSoapAPIInterceptor } from '@test-utils/network/msw/create-api-interceptor';
+
+const apiAction = 'FolderAction';
+
+export const mockRestoreCalendarApiOk = (
+	response = { _jsns: 'urn:zimbraMail' as const }
+): Promise<FolderActionRequest> =>
+	createSoapAPIInterceptor<FolderActionRequest, FolderActionResponseOk>(apiAction, response);
---
+++ src/integration-tests/restore-calendar.test.ts
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { screen } from '@testing-library/react';
+import { FOLDERS } from '@zextras/carbonio-ui-commons';
+
+import { mockExpandedFolders, setupIntegrationTest } from './utils';
+import { SIDEBAR_ROOT_SUBSECTION } from '../constants/sidebar';
+import { UserEvent } from '@test-setup';
+import { mockRestoreCalendarApiOk } from '@test-utils/api/restore-calendar';
+import { generateFolder } from '@test-utils/folders/folders-generator';
+
+async function restoreCalendar(user: UserEvent, element: HTMLElement): Promise<void> {
+	await user.rightClick(element);
+	const restoreCalendarAction = await screen.findByText('label.restore_calendar');
+	await user.click(restoreCalendarAction);
+}
+describe('Restore Calendar Integration Tests', () => {
+	beforeAll(() => {
+		mockExpandedFolders([FOLDERS.USER_ROOT, FOLDERS.TRASH, SIDEBAR_ROOT_SUBSECTION.CALENDARS]);
+	});
+	it('should restore calendar when confirming restore', async () => {
+		const trashedCalendar = generateFolder({
+			name: 'Trashed Calendar',
+			id: 'my-trashed-calendar',
+			// fields required to make restore work
+			parent: FOLDERS.TRASH,
+			absFolderPath: '/Trash/trashed-folder',
+			depth: 2
+		});
+		const user = await setupIntegrationTest({ calendar: trashedCalendar });
+
+		const myFolderElement = await screen.findByText('Trashed Calendar');
+
+		const restoreCalendarApi = mockRestoreCalendarApiOk();
+		await restoreCalendar(user, myFolderElement);
+
+		const request = await restoreCalendarApi;
+		expect(request.action.op).toBe('move');
+		expect(request.action.id).toBe(trashedCalendar.id);
+		expect(request.action.l).toBe(FOLDERS.USER_ROOT);
+	});
+});
