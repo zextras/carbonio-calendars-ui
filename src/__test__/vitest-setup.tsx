bc0959e678663aa11c0f35c06000937bd702ee09
Dennis <dennis.ceron@zextras.com>
2025-12-05 11:01:36 +0100

    fix: set fake timer default in test configuration

--- src/__test__/vitest-setup.tsx
+++ src/__test__/vitest-setup.tsx
@@ -204,6 +204,7 @@ beforeEach(() => {
 	});
 
 	vi.useFakeTimers({ shouldAdvanceTime: true });
+	vi.setSystemTime(new Date('1970-01-01T00:00:00.000Z'));
 	// useDistributionListsStore.getState().reset();
 });
 
