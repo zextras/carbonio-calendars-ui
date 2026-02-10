420ede5e453d08fcea5f57328e2bb3ffdd3622d8
Keshav Bhatt <keshavnrj@gmail.com>
2026-02-10 19:00:23 +0530

    fix: remove duplicate imports in custom-recurrence-modal.tsx

--- src/view/editor/parts/recurrence/views/custom-recurrence-modal.tsx
+++ src/view/editor/parts/recurrence/views/custom-recurrence-modal.tsx
@@ -7,6 +7,7 @@ import React, { ReactElement, useCallback, useState } from 'react';
 
 import { Divider, ModalBody, ModalHeader, Padding, Text } from '@zextras/carbonio-design-system';
 import { t } from '@zextras/carbonio-shell-ui';
+import { ModalFooter } from '@zextras/carbonio-ui-commons';
 import { isNil, omitBy } from 'lodash';
 
 import DailyOptions from './daily-options';
@@ -14,6 +15,7 @@ import MonthlyOptions from './monthly-options';
 import RecurrenceEndOptions from './recurrence-end-options';
 import WeeklyOptions from './weekly-options';
 import YearlyOptions from './yearly-options';
+import FrequencySelect from '../components/frequency-select';
 import { RecurrenceContext } from 'commons/recurrence-context';
 import { useAppDispatch, useAppSelector } from 'store/redux/hooks';
 import {
@@ -23,8 +25,6 @@ import {
 } from 'store/selectors/editor';
 import { editEditorRecurrence } from 'store/slices/editor-slice';
 import { RecurrenceEndValue, RecurrenceStartValue } from 'types/editor';
-import FrequencySelect from '../components/frequency-select';
-import { ModalFooter } from '@zextras/carbonio-ui-commons';
 
 const setEndInitialValue = (
 	count: number | undefined,
