591cd38452d4c53f2455b93d115e75b5ff4144a6
Keshav Bhatt <keshavnrj@gmail.com>
2026-02-13 17:07:14 +0530

    refactor: streamline state initialization in CustomRecurrenceModal

--- src/view/editor/parts/recurrence/views/custom-recurrence-modal.tsx
+++ src/view/editor/parts/recurrence/views/custom-recurrence-modal.tsx
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { ReactElement, useCallback, useMemo, useState } from 'react';
+import React, { ReactElement, useCallback, useState } from 'react';
 
 import { Divider, ModalBody, ModalHeader, Padding, Text } from '@zextras/carbonio-design-system';
 import { t } from '@zextras/carbonio-shell-ui';
@@ -16,12 +16,8 @@ import { WeeklyOptions } from './weekly-options';
 import { RecurrenceContext } from 'commons/recurrence-context';
 import { useAppDispatch, useAppSelector } from 'store/redux/hooks';
 import {
-	selectEditorRecurrence,
-	selectEditorRecurrenceByDay,
 	selectEditorRecurrenceCount,
 	selectEditorRecurrenceFrequency,
-	selectEditorRecurrenceInterval,
-	selectEditorRecurrenceSetPos,
 	selectEditorRecurrenceUntilDate
 } from 'store/selectors/editor';
 import { editEditorRecurrence } from 'store/slices/editor-slice';
@@ -54,68 +50,20 @@ export const CustomRecurrenceModal = ({
 	const freq = useAppSelector(selectEditorRecurrenceFrequency(editorId));
 	const count = useAppSelector(selectEditorRecurrenceCount(editorId));
 	const until = useAppSelector(selectEditorRecurrenceUntilDate(editorId));
-	const interval = useAppSelector(selectEditorRecurrenceInterval(editorId));
-	const byday = useAppSelector(selectEditorRecurrenceByDay(editorId));
-	const bysetpos = useAppSelector(selectEditorRecurrenceSetPos(editorId));
-	const recur = useAppSelector(selectEditorRecurrence(editorId));
 	const dispatch = useAppDispatch();
 
 	const [frequency, setFrequency] = useState(freq);
-
-	// Initialize context state from editor recurrence data
-	const initialStartValue = useMemo<RecurrenceStartValue | undefined>(() => {
-		if (!recur) return undefined;
-
-		const rule = recur?.[0]?.add?.[0]?.rule?.[0];
-		if (!rule) return undefined;
-
-		const startValue: RecurrenceStartValue = {};
-
-		// Add interval if it exists
-		if (interval) {
-			startValue.interval = interval;
-		}
-
-		// Add byday if it exists
-		if (byday) {
-			startValue.byday = byday;
-		}
-
-		// Add bysetpos if it exists
-		if (bysetpos) {
-			startValue.bysetpos = { poslist: bysetpos };
-		}
-
-		// Add bymonthday if it exists
-		const bymonthday = rule?.bymonthday?.[0]?.modaylist;
-		if (bymonthday !== undefined) {
-			startValue.bymonthday = { modaylist: bymonthday };
-		}
-
-		// Add bymonth if it exists
-		const bymonth = rule?.bymonth?.[0]?.molist;
-		if (bymonth !== undefined) {
-			startValue.bymonth = { molist: bymonth };
-		}
-
-		return Object.keys(startValue).length > 0 ? startValue : undefined;
-	}, [recur, interval, byday, bysetpos]);
-
-	const [newStartValue, setNewStartValue] = useState<RecurrenceStartValue | undefined>(
-		initialStartValue
-	);
+	const [newStartValue, setNewStartValue] = useState<RecurrenceStartValue>();
 	const [newEndValue, setNewEndValue] = useState(() => setEndInitialValue(count, until));
 
 	const onConfirm = useCallback(() => {
-		const recurrence = {
+		const recur = {
 			add: {
 				rule: omitBy({ ...(newStartValue ?? {}), ...(newEndValue ?? {}), freq: frequency }, isNil)
 			}
 		};
-		dispatch(editEditorRecurrence({ id: editorId, recur: recurrence }));
+		dispatch(editEditorRecurrence({ id: editorId, recur }));
 		onClose();
-		setNewStartValue(undefined);
-		setNewEndValue(undefined);
 	}, [newStartValue, newEndValue, frequency, dispatch, editorId, onClose]);
 
 	const recurrenceContextValue = {
