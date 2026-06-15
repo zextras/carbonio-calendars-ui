7c0215d2b520aae3000fc9327f28bd6d58d38d67
Gabriele Marino <gabriele.marino@zextras.com>
2026-06-15 13:04:44 +0200

    refactor(CO-3651): replace instanceData string concatenation with localized i18n keys
    Use message.event_instance_data and message.event_series_data with {{title}} and {{date}}
    interpolation so translators can freely reorder the sentence structure per language

--- src/store/actions/move-appointment-to-trash.ts
+++ src/store/actions/move-appointment-to-trash.ts
@@ -5,7 +5,7 @@
  */
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { TFunction } from 'i18next';
-import { compact, lowerCase } from 'lodash';
+import { compact } from 'lodash';
 
 import { getTimeStrings } from '../../hooks/use-get-date-range-converted-to-timezone';
 import {
@@ -72,12 +72,16 @@ export const buildMessagePart = ({
 			timeZone: inst?.tz ?? fullInvite?.tz
 		}
 	});
-	const instance =
+	const instanceData =
 		!fullInvite.recurrenceRule || deleteSingleInstance
-			? t('label.instance', 'instance')
-			: t('label.series', 'series');
-
-	const instanceData = `"${fullInvite?.name ?? ''}" ${lowerCase(instance)}, ${date}`;
+			? t('message.event_instance_data', '"{{title}}" instance, {{date}}', {
+					title: fullInvite?.name ?? '',
+					date
+				})
+			: t('message.event_series_data', '"{{title}}" series, {{date}}', {
+					title: fullInvite?.name ?? '',
+					date
+				});
 
 	const originalContentPlainSection = originalInviteContentPlain
 		? `\n\n${originalInviteTitle}\n\n${originalInviteContentPlain}`
